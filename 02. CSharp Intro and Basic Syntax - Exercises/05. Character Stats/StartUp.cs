b4c8f594ab2a0b16b9a3d82b794189d0240abfb2
Teodor <thelad43@gmail.com>
2017-12-23 13:40:46 +0200

    Added solution for problem 05. Character Stats

--- 02. CSharp Intro and Basic Syntax - Exercises/05. Character Stats/StartUp.cs
+++ 02. CSharp Intro and Basic Syntax - Exercises/05. Character Stats/StartUp.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.Character_Stats
+﻿namespace _05.Character_Stats
 {
-    class Program
+    using System;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maxHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: {new string('|', currentHealth + 1)}{new string('.', maxHealth - currentHealth)}|");
+            Console.WriteLine($"Energy: {new string('|', currentEnergy + 1)}{new string('.', maxEnergy - currentEnergy)}|");
         }
     }
 }
