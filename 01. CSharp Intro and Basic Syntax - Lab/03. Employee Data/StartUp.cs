fcfa4a74c78a92b9dce1fa8fbe405efe70b8aa1e
Teodor <thelad43@gmail.com>
2017-12-23 12:10:59 +0200

    Added solution for problem 03. Employee Data

---
+++ 01. CSharp Intro and Basic Syntax - Lab/03. Employee Data/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Employee_Data
+{
+    using System;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = Console.ReadLine();
+            var id = int.Parse(Console.ReadLine());
+            var salary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {id.ToString("D8", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Salary: {salary:F2}");
+        }
+    }
+}
