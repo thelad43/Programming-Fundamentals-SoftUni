bea5559a78f7e70b0c98753a9ed3b7fb9b5b1da9
Teodor <thelad43@gmail.com>
2017-12-23 12:09:46 +0200

    Added solution for problem 02. Add Two Numbers

--- 01. CSharp Intro and Basic Syntax - Lab/02. Add Two Numbers/Properties/AssemblyInfo.cs
+++ 01. CSharp Intro and Basic Syntax - Lab/02. Add Two Numbers/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01. Greeting")]
+[assembly: AssemblyTitle("02. Add Two Numbers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01. Greeting")]
+[assembly: AssemblyProduct("02. Add Two Numbers")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0275b28f-3391-41ed-b87e-4efe1452b4d1")]
+[assembly: Guid("be57eb7f-61d0-48ad-8ed9-a6a1a4c31fe0")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ 01. CSharp Intro and Basic Syntax - Lab/02. Add Two Numbers/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.Add_Two_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
+        }
+    }
+}
