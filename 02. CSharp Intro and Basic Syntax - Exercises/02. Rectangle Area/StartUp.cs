4b607795edc43fa8a7dd7136e38d8bd505716d78
Teodor <thelad43@gmail.com>
2017-12-23 13:32:08 +0200

    Added solution for problem 02. Rectangle Area

---
+++ 02. CSharp Intro and Basic Syntax - Exercises/02. Rectangle Area/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.Rectangle_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var height = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(height * width):F2}");
+        }
+    }
+}
