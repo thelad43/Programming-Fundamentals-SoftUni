9ab32f1b67b34edd10e63681de13523951cf02c9
Teodor <thelad43@gmail.com>
2017-12-23 12:06:55 +0200

    Added solution for problem 01. Greeting

---
+++ 01. CSharp Intro and Basic Syntax - Lab/01. Greeting/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.Greeting
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
