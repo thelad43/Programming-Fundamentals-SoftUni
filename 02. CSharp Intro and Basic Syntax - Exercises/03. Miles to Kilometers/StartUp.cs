9314f626b19a678390f8f6a807808b718be6e30f
Teodor <thelad43@gmail.com>
2017-12-23 13:37:01 +0200

    Added solution for problem 03. Miles to Kilometers

---
+++ 02. CSharp Intro and Basic Syntax - Exercises/03. Miles to Kilometers/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _03.Miles_to_Kilometers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var miles = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(miles * 1.60934):F2}");
+        }
+    }
+}
