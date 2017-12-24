091325d002b119e8c9ff81c0f98d27ff23ca55e6
Teodor <thelad43@gmail.com>
2017-12-24 11:20:00 +0200

    Added solution for problem 01. Passed

---
+++ 03. CSharp Conditional Statements and Loops - Lab/01. Passed/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.Passed
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
