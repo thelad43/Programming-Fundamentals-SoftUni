23b0e2378fa64710ced4909c3c067d37425c891d
Teodor <thelad43@gmail.com>
2017-12-24 11:32:01 +0200

    Added solution for problem 11. Odd Number

---
+++ 03. CSharp Conditional Statements and Loops - Lab/11. Odd Number/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _11.Odd_Number
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            while (n % 2 == 0)
+            {
+                Console.WriteLine("Please write an odd number.");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The number is: {Math.Abs(n)}");
+        }
+    }
+}
