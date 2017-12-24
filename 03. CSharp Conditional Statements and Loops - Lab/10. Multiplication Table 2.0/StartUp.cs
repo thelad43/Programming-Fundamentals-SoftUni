24d7e1d6c1d41cf3e7064dc87333fcd4821d2739
Teodor <thelad43@gmail.com>
2017-12-24 11:30:59 +0200

    Added solution for problem 10. Multiplication Table 2.0

---
+++ 03. CSharp Conditional Statements and Loops - Lab/10. Multiplication Table 2.0/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.Multiplication_Table_2._0
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var times = int.Parse(Console.ReadLine());
+            var isEnteredLoop = false;
+
+            for (int i = times; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+                isEnteredLoop = true;
+            }
+
+            if (!isEnteredLoop)
+            {
+                Console.WriteLine($"{n} X {times} = {n * times}");
+            }
+        }
+    }
+}
