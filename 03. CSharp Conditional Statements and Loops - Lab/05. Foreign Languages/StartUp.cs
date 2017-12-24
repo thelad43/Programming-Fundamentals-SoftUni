11a23dd6f2e9efc5542bf13601b3c8fe04eea88b
Teodor <thelad43@gmail.com>
2017-12-24 11:24:30 +0200

    Added solution for problem 05. Foreign Languages

---
+++ 03. CSharp Conditional Statements and Loops - Lab/05. Foreign Languages/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.Foreign_Languages
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            switch (Console.ReadLine())
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
