5ac6970bf0861c57f0fec8bf0e4df355ec38d0a4
Teodor <thelad43@gmail.com>
2017-12-24 11:25:29 +0200

    Added solution for problem 06. Theatre Promotion

---
+++ 03. CSharp Conditional Statements and Loops - Lab/06. Theatre Promotion/StartUp.cs
@@ -0,0 +1,81 @@
+﻿namespace _06.Theatre_Promotion
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var typeOfDay = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var result = 0M;
+
+            if (age < 0)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            switch (typeOfDay)
+            {
+                case "Weekday":
+                    if (age <= 18)
+                    {
+                        result = 12;
+                    }
+                    else if (age <= 64)
+                    {
+                        result = 18;
+                    }
+                    else if (age <= 122)
+                    {
+                        result = 12;
+                    }
+                    break;
+
+                case "Weekend":
+                    if (age <= 18)
+                    {
+                        result = 15;
+                    }
+                    else if (age <= 64)
+                    {
+                        result = 20;
+                    }
+                    else if (age <= 122)
+                    {
+                        result = 15;
+                    }
+                    break;
+
+                case "Holiday":
+                    if (age <= 18)
+                    {
+                        result = 5;
+                    }
+                    else if (age <= 64)
+                    {
+                        result = 12;
+                    }
+                    else if (age <= 122)
+                    {
+                        result = 10;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Error!");
+                    return;
+            }
+
+            if (result > 0)
+            {
+                Console.WriteLine($"{result}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
