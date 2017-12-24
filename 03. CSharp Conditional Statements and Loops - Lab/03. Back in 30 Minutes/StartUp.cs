0beb128e1885437296b4d7ab0363aa0db7d6c8df
Teodor <thelad43@gmail.com>
2017-12-24 11:22:25 +0200

    Added solution for problem 03. Back in 30 Minutes

---
+++ 03. CSharp Conditional Statements and Loops - Lab/03. Back in 30 Minutes/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.Back_in_30_Minutes
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var mins = int.Parse(Console.ReadLine());
+
+            var totalMins = hours * 60 + mins + 30;
+            var totalHours = (totalMins / 60);
+            var resultMins = totalMins % 60;
+
+            if (totalHours == 24 && resultMins < 10)
+            {
+                Console.WriteLine($"0:0{resultMins}");
+            }
+            else if (totalHours == 24 && resultMins > 9)
+            {
+                Console.WriteLine($"0:{resultMins}");
+            }
+            else if (resultMins < 10)
+            {
+                Console.WriteLine($"{totalHours}:0{resultMins}");
+            }
+            else
+            {
+                Console.WriteLine($"{totalHours}:{resultMins}");
+            }
+        }
+    }
+}
