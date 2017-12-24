0f517f048b9b5e73c6b07385172ca4d0701552cc
Teodor <thelad43@gmail.com>
2017-12-24 11:33:00 +0200

    Added solution for problem 12. Number Checker

---
+++ 03. CSharp Conditional Statements and Loops - Lab/12. Number Checker/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _12.Number_Checker
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            if (double.TryParse(input, out var num))
+            {
+                Console.WriteLine("It is a number.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
