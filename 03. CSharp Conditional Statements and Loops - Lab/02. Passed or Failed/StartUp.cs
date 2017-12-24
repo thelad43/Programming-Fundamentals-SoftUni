655818ad85a18585f2d82ebb326ffe6428b15f66
Teodor <thelad43@gmail.com>
2017-12-24 11:21:16 +0200

    Added solution for problem 02. Passed or Failed

---
+++ 03. CSharp Conditional Statements and Loops - Lab/02. Passed or Failed/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.Passed_or_Failed
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade < 3)
+            {
+                Console.WriteLine("Failed!");
+            }
+            else
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
