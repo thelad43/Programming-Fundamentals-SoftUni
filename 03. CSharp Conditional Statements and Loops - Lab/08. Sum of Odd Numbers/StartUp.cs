697dcd380de905a78d3a32008a5553e3f4b652ec
Teodor <thelad43@gmail.com>
2017-12-24 11:28:05 +0200

    Added solution for problem 08. Sum of Odd Numbers

---
+++ 03. CSharp Conditional Statements and Loops - Lab/08. Sum of Odd Numbers/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.Sum_of_Odd_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var x = 1;
+
+            for (int i = 1; x <= n; i += 2)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    x++;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
