4568326f711122eadc3bf90ea8199752aa4af8b5
Teodor <thelad43@gmail.com>
2017-12-26 14:08:11 +0200

    05. Word in Plural

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/05. Word in Plural/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.Word_in_Plural
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var word = Console.ReadLine();
+
+            if (word.EndsWith("y"))
+            {
+                word = word.TrimEnd('y');
+                word = word.Insert(word.Length, "ies");
+            }
+            else if (word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") || word.EndsWith("sh") || word.EndsWith("x") || word.EndsWith("z"))
+            {
+                word = word.Insert(word.Length, "es");
+            }
+            else
+            {
+                word = word.Insert(word.Length, "s");
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
