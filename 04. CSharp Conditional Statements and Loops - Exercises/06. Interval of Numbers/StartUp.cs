60a5978a2936eee4029cb226b9d0e2cd99b1fa0b
Teodor <thelad43@gmail.com>
2017-12-26 14:09:18 +0200

    Added solution for problem 06. Interval of Numbers

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/06. Interval of Numbers/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.Interval_of_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+
+            if (firstNum > secondNum)
+            {
+                var oldValue = firstNum;
+                firstNum = secondNum;
+                secondNum = oldValue;
+            }
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
