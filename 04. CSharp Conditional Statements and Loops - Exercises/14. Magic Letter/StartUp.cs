670a44773eb614bc6adb83eb9c04b1fc3c1fd7d1
Teodor <thelad43@gmail.com>
2017-12-26 14:18:18 +0200

    Added solution for problem 14. Magic Letter

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/14. Magic Letter/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _14.Magic_Letter
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var thirdLetter = Console.ReadLine();
+
+            var result = string.Empty;
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        result = $"{i}{j}{k}";
+
+                        if (!result.Contains(thirdLetter))
+                        {
+                            Console.Write($"{result} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
