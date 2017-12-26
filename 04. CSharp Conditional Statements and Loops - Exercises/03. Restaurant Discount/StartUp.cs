eb948dc6d3418151a4f2807fbb52bb638658c39f
Teodor <thelad43@gmail.com>
2017-12-26 14:06:11 +0200

    Added solution for problem 03. Restaurant Discount

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/03. Restaurant Discount/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace _03.Restaurant_Discount
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var groupSize = int.Parse(Console.ReadLine());
+            var package = Console.ReadLine();
+
+            decimal price;
+            string hallName;
+
+            if (groupSize <= 50)
+            {
+                price = 2500;
+                hallName = "Small Hall";
+            }
+            else if (groupSize > 50 && groupSize <= 100)
+            {
+                price = 5000;
+                hallName = "Terrace";
+            }
+            else if (groupSize <= 120)
+            {
+                price = 7500;
+                hallName = "Great Hall";
+            }
+            else
+            {
+                Console.WriteLine("We do not have an appropriate hall.");
+                return;
+            }
+
+            switch (package)
+            {
+                case "Normal":
+                    price += 500;
+                    price = price - (price * 0.05M);
+                    break;
+
+                case "Gold":
+                    price += 750;
+                    price = price - (price * 0.10M);
+                    break;
+
+                case "Platinum":
+                    price += 1000;
+                    price = price - (price * 0.15M);
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            var pricePerPerson = price / groupSize;
+
+            Console.WriteLine($"We can offer you the {hallName}");
+            Console.WriteLine($"The price per person is {pricePerPerson:F2}$");
+        }
+    }
+}
