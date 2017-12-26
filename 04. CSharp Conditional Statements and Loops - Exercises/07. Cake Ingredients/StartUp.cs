bdcb41fea1fcf5f925e851a15832acaa693fbb86
Teodor <thelad43@gmail.com>
2017-12-26 14:10:30 +0200

    Added solution for problem 07. Cake Ingredients

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/07. Cake Ingredients/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.Cake_Ingredients
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var ingredients = 0;
+
+            while (inputLine != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {inputLine}.");
+                ingredients++;
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Preparing cake with {ingredients} ingredients.");
+        }
+    }
+}
