9177cd0a7c82a1548b8595059f84ac297787657f
Teodor <thelad43@gmail.com>
2017-12-26 14:13:47 +0200

    Added solution for problem 09. Count the Integers

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/09. Count the Integers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.Count_the_Integers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var countOfInts = 0;
+
+            while (int.TryParse(input, out var n))
+            {
+                countOfInts++;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(countOfInts);
+        }
+    }
+}
