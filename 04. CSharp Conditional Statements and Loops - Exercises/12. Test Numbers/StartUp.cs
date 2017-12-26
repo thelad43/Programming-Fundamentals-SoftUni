6b57a6c25ab59371c556e2a12f4ad0ff2eb0fbe6
Teodor <thelad43@gmail.com>
2017-12-26 14:16:30 +0200

    Added solution for problem 12. Test Numbers

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/12. Test Numbers/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _12.Test_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var maximumSumBoundary = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+            var totalCombinations = 0;
+            var isMagicNumber = false;
+
+            for (int i = a; i >= 1; i--)
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    if (sum >= maximumSumBoundary)
+                    {
+                        isMagicNumber = true;
+                        break;
+                    }
+                    else
+                    {
+                        totalCombinations++;
+                        sum += 3 * (i * j);
+                    }
+                }
+
+                if (sum >= maximumSumBoundary)
+                {
+                    isMagicNumber = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{totalCombinations} combinations");
+
+            if (isMagicNumber)
+            {
+                Console.WriteLine($"Sum: {sum} >= {maximumSumBoundary }");
+            }
+            else
+            {
+                Console.WriteLine($"Sum: {sum}");
+            }
+        }
+    }
+}
