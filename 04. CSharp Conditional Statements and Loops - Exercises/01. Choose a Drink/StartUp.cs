06e79b1e5f2aac9fa0630b6837425148c6f2e384
Teodor <thelad43@gmail.com>
2017-12-26 14:04:03 +0200

    Added solution for problem 01. Choose a Drink

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/01. Choose a Drink/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.Choose_a_Drink
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var profession = Console.ReadLine();
+
+            switch (profession)
+            {
+                case "Athlete":
+                    Console.WriteLine("Water");
+                    break;
+
+                case "Businessman":
+                case "Businesswoman":
+                    Console.WriteLine("Coffee");
+                    break;
+
+                case "SoftUni Student":
+                    Console.WriteLine("Beer");
+                    break;
+
+                default:
+                    Console.WriteLine("Tea");
+                    break;
+            }
+        }
+    }
+}
