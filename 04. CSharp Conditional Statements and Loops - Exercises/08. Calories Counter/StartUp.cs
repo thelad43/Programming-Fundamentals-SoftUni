7c88fc91aa0bbb5f6309dc1034785ed3655ec2a0
Teodor <thelad43@gmail.com>
2017-12-26 14:11:38 +0200

    Added solution for problem 08. Calories Counter

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/08. Calories Counter/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _08.Calories_Counter
+{
+    using System;
+
+    public class StartUp
+    {
+        private const int caloriesInCheese = 500;
+        private const int caloriesInTomatoSauce = 150;
+        private const int caloriesInSalami = 600;
+        private const int caloriesInPepper = 50;
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            string ingredient;
+            var calories = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                ingredient = Console.ReadLine().ToLower();
+
+                switch (ingredient)
+                {
+                    case "cheese":
+                        calories += caloriesInCheese;
+                        break;
+
+                    case "tomato sauce":
+                        calories += caloriesInTomatoSauce;
+                        break;
+
+                    case "salami":
+                        calories += caloriesInSalami;
+                        break;
+
+                    case "pepper":
+                        calories += caloriesInPepper;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Total calories: {calories}");
+        }
+    }
+}
