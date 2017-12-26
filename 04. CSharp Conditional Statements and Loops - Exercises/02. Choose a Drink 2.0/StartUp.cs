2efd5c227669a09daa7f97e341ad84a82202af4d
Teodor <thelad43@gmail.com>
2017-12-26 14:05:08 +0200

    Added solution for problem 02. Choose a Drink 2.0

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/02. Choose a Drink 2.0/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.Choose_a_Drink_2._0
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var profession = Console.ReadLine();
+            var quantity = int.Parse(Console.ReadLine());
+
+            switch (profession)
+            {
+                case "Athlete":
+                    Console.WriteLine($"The {profession} has to pay {(quantity * 0.7):F2}.");
+                    break;
+
+                case "Businessman":
+                case "Businesswoman":
+                    Console.WriteLine($"The {profession} has to pay {(quantity * 1.0):F2}.");
+                    break;
+
+                case "SoftUni Student":
+                    Console.WriteLine($"The {profession} has to pay {(quantity * 1.7):F2}.");
+                    break;
+
+                default:
+                    Console.WriteLine($"The {profession} has to pay {(quantity * 1.2):F2}.");
+                    break;
+            }
+        }
+    }
+}
