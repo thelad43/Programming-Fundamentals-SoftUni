6c7e98fc15f805ead514ef90f6d6f8c0669e747b
Teodor <thelad43@gmail.com>
2017-12-26 14:17:25 +0200

    Added solution for problem 13. Game of Numbers

---
+++ 04. CSharp Conditional Statements and Loops - Exercises/13. Game of Numbers/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace _13.Game_of_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var magicalNumber = int.Parse(Console.ReadLine());
+
+            var counter = 0;
+            var firstDigit = 0;
+            var secondDigit = 0;
+            var magicalNumberFound = false;
+
+            for (int i = n; i <= m; i++)
+            {
+                for (int j = n; j <= m; j++)
+                {
+                    counter++;
+
+                    if (i + j == magicalNumber)
+                    {
+                        firstDigit = i;
+                        secondDigit = j;
+                        magicalNumberFound = true;
+                    }
+                }
+            }
+
+            if (magicalNumberFound)
+            {
+                Console.WriteLine($"Number found! {firstDigit} + {secondDigit} = {magicalNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicalNumber}");
+            }
+        }
+    }
+}
