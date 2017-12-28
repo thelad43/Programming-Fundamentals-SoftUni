2589ed021fef36a7ca03dbe89afc5bb313b360a1
Teodor <thelad43@gmail.com>
2017-12-28 16:08:38 +0200

    Added solution for problem 02. Circle Area (Precision 12)

---
+++ 05. Data Types and Variables - Lab/02. Circle Area (Precision 12)/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.Circle_Area__Precision_12_
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(Math.PI * radius * radius):F12}");
+        }
+    }
+}
