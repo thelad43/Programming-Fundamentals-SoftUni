245e9a277aadb321da0f9dbdd2a50dd868a8b662
Teodor <thelad43@gmail.com>
2017-12-28 16:13:50 +0200

    Added solution for problem 08. Refactor Volume of Pyramid

---
+++ 05. Data Types and Variables - Lab/08. Refactor Volume of Pyramid/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.Refactor_Volume_of_Pyramid
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length, width, heigth = 0;
+
+            Console.Write("Length: ");
+            length = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            heigth = double.Parse(Console.ReadLine());
+
+            var volume = (length * width * heigth) / 3.0;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+        }
+    }
+}
