0c58ac5d874ebd111f78f85088d7098b14a09a86
Teodor <thelad43@gmail.com>
2017-12-28 16:07:36 +0200

    Added solution for problem 01. Centuries to Minutes

---
+++ 05. Data Types and Variables - Lab/01. Centuries to Minutes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Centuries_to_Minutes
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var centuries = int.Parse(Console.ReadLine());
+            var years = centuries * 100;
+            var days = (int)(years * 365.2422);
+            var hours = days * 24;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {hours * 60} minutes");
+        }
+    }
+}
