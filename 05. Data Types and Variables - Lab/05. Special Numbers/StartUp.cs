f2e659a466072680a8fa2b47e9b8a10a4d0e8c29
Teodor <thelad43@gmail.com>
2017-12-28 16:11:06 +0200

    Added solution for problem 05. Special Numbers

---
+++ 05. Data Types and Variables - Lab/05. Special Numbers/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.Special_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                var sum = 0;
+                var num = i;
+
+                while (num != 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+
+                var isSpecial = sum == 5 || sum == 7 || sum == 11 ? true : false;
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
