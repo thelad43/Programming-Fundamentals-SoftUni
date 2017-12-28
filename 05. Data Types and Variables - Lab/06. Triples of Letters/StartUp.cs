623b57b884d81a4d4d39fac32f8fd4f3b11900db
Teodor <thelad43@gmail.com>
2017-12-28 16:11:57 +0200

    Added solution for problem 06. Triples of Letters

---
+++ 05. Data Types and Variables - Lab/06. Triples of Letters/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.Triples_of_Letters
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.WriteLine($"{(char)('a' + i)}{(char)('a' + j)}{(char)('a' + k)} ");
+                    }
+                }
+            }
+        }
+    }
+}
