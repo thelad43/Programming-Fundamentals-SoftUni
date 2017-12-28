c63e8c013e7c87d20433105f549b2df00d8d1c00
Teodor <thelad43@gmail.com>
2017-12-28 16:14:42 +0200

    Added solution for problem 09. Refactor Special Numbers

---
+++ 05. Data Types and Variables - Lab/09. Refactor Special Numbers/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _09.Refactor_Special_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var number = 0;
+            var isSpecial = false;
+            var sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                number = i;
+
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{number} -> {isSpecial}");
+                sum = 0;
+                i = number;
+            }
+        }
+    }
+}
