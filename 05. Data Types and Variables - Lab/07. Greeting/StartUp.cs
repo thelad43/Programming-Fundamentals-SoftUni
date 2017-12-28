eabb960995504bc1f0bf49fa10b42c01feb74be3
Teodor <thelad43@gmail.com>
2017-12-28 16:13:00 +0200

    Added solution for problem 07. Greeting

---
+++ 05. Data Types and Variables - Lab/07. Greeting/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _07.Greeting
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstName = Console.ReadLine();
+            var lastName = Console.ReadLine();
+            var age = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName} {lastName}. You are {age} years old.");
+        }
+    }
+}
