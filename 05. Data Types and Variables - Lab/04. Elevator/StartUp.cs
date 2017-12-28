1113da40b04328ca2da454cde0c5864ded4fa859
Teodor <thelad43@gmail.com>
2017-12-28 16:10:16 +0200

    Added solution for problem 04. Elevator

---
+++ 05. Data Types and Variables - Lab/04. Elevator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.Elevator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var p = int.Parse(Console.ReadLine());
+            var courses = (int)Math.Ceiling((double)n / p);
+            Console.WriteLine(courses);
+        }
+    }
+}
