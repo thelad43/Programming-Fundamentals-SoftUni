b9848e291f4e4a7f8500af2d22a7c9c0d2a065bf
Teodor <thelad43@gmail.com>
2017-12-28 16:09:30 +0200

    Added solution for problem 03. Exact Sum of Real Numbers

---
+++ 05. Data Types and Variables - Lab/03. Exact Sum of Real Numbers/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.Exact_Sum_of_Real_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0M;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += decimal.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
