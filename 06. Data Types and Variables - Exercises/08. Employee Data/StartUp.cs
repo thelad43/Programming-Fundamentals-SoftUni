89ed6a6c32456e7cd8ec124dc3c742bd26fe5b5f
Teodor <thelad43@gmail.com>
2017-12-30 15:49:18 +0200

    Added solution for problem 08. Employee Data

---
+++ 06. Data Types and Variables - Exercises/08. Employee Data/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.Employee_Data
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstName = Console.ReadLine();
+            var lastName = Console.ReadLine();
+            var age = Console.ReadLine();
+            var gender = Console.ReadLine();
+            var id = Console.ReadLine();
+            var uniqueEmployeeNumber = Console.ReadLine();
+
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {id}");
+            Console.WriteLine($"Unique Employee number: {uniqueEmployeeNumber}");
+        }
+    }
+}
