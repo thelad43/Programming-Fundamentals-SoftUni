9090d36be64041b44bf77cffd6fc117d3855266b
Teodor <thelad43@gmail.com>
2017-12-30 15:53:45 +0200

    Added solution for problem 12. Rectangle Properties

---
+++ 06. Data Types and Variables - Exercises/12. Rectangle Properties/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _12.Rectangle_Properties
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            var perimeter = 2 * (a + b);
+            var area = a * b;
+            var diagonal = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
+        }
+    }
+}
