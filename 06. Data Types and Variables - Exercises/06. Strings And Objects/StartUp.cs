97b734eb10a167d4d58e7aa948cf95428b586723
Teodor <thelad43@gmail.com>
2017-12-30 15:46:57 +0200

    Added solution for problem 06. Strings And Objects

---
+++ 06. Data Types and Variables - Exercises/06. Strings And Objects/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _06.Strings_And_Objects
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string str1 = "Hello";
+            string str2 = "World";
+            object obj = str1 + " " + str2;
+            string str3 = (string)obj;
+            Console.WriteLine(str3);
+        }
+    }
+}
