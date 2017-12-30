0b56d97bd2e84bc513c0a630612563274ffb00c6
Teodor <thelad43@gmail.com>
2017-12-30 15:42:43 +0200

    Added solution for problem 03. Practice Chars And Strings

---
+++ 06. Data Types and Variables - Exercises/03. Practice Chars And Strings/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.Practice_Chars_And_Strings
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var str1 = "Software University";
+            var char1 = 'B';
+            var char2 = 'y';
+            var char3 = 'e';
+            var str2 = "I love programming";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(char1);
+            Console.WriteLine(char2);
+            Console.WriteLine(char3);
+            Console.WriteLine(str2);
+        }
+    }
+}
