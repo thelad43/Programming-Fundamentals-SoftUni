302dacced41483d0631e77cb8586d850e684d011
Teodor <thelad43@gmail.com>
2017-12-30 15:54:37 +0200

    Added solution for problem 13. Vowel or Digit

---
+++ 06. Data Types and Variables - Exercises/13. Vowel or Digit/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _13.Vowel_or_Digit
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            bool parsed;
+            parsed = char.TryParse(Console.ReadLine().ToLower(), out var input);
+
+            if (!parsed)
+            {
+                Console.WriteLine("other");
+                return;
+            }
+
+            if (char.IsDigit(input))
+            {
+                Console.WriteLine("digit");
+            }
+            else if (IsVowel(input))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+
+        private static bool IsVowel(char input)
+        {
+            if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u')
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
