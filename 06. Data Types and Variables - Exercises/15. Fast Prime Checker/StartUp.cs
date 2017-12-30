4a4ebf37d36f6d54b27bdfeaacf32530786c1e2d
Teodor <thelad43@gmail.com>
2017-12-30 15:56:22 +0200

    Added solution for problem 15. Fast Prime Checker

---
+++ 06. Data Types and Variables - Exercises/15. Fast Prime Checker/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _15.Fast_Prime_Checker
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 2; currentNumber <= num; currentNumber++)
+            {
+                bool isPrime = true;
+
+                for (int devider = 2; devider <= Math.Sqrt(currentNumber); devider++)
+                {
+                    if (currentNumber % devider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{currentNumber } -> {isPrime}");
+            }
+        }
+    }
+}
