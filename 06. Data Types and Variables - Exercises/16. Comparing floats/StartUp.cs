03df077f7500428db5f358e8ac1798b17b97114c
Teodor <thelad43@gmail.com>
2017-12-30 15:57:36 +0200

    Added solution for problem 16. Comparing floats

---
+++ 06. Data Types and Variables - Exercises/16. Comparing floats/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _16.Comparing_floats
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var eps = 0.000001;
+
+            if (Math.Abs(a - b) < eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
