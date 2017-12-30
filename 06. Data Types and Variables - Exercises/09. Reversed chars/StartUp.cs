6ba50250e454ce4ce73fe777d30674ffb5bd9f6d
Teodor <thelad43@gmail.com>
2017-12-30 15:50:13 +0200

    Added solution for problem 09. Reversed chars

---
+++ 06. Data Types and Variables - Exercises/09. Reversed chars/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.Reversed_chars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var thirdLetter = char.Parse(Console.ReadLine());
+
+            Console.Write(thirdLetter);
+            Console.Write(secondLetter);
+            Console.WriteLine(firstLetter);
+        }
+    }
+}
