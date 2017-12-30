5ba6539022708f2acc597aa2ecee6b0e437c674b
Teodor <thelad43@gmail.com>
2017-12-30 15:41:37 +0200

    Added solution for problem 02. Practice Floating Points

---
+++ 06. Data Types and Variables - Exercises/02. Practice Floating Points/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.Practice_Floating_Points
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num1 = 3.141592653589793238M;
+            var num2 = 1.60217657D;
+            var num3 = 7.8184261974584555216535342341M;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+        }
+    }
+}
