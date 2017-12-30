86fa70c2518211363704146160e04e1d16186426
Teodor <thelad43@gmail.com>
2017-12-30 16:00:36 +0200

    Added solution for problem 19. Thea The Photographer

---
+++ 06. Data Types and Variables - Exercises/19. Thea The Photographer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _19.Thea_The_Photographer
+{
+    using System;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var pics = int.Parse(Console.ReadLine());
+            var filterTime = int.Parse(Console.ReadLine());
+            var filterFactor = int.Parse(Console.ReadLine());
+            var uploadTime = int.Parse(Console.ReadLine());
+
+            var percentage = filterFactor / 100.0;
+            var usefulPictures = (int)Math.Ceiling(pics * percentage);
+
+            var totalPictures = pics * (long)filterTime;
+            var uploadedPictures = usefulPictures * (long)uploadTime;
+
+            Console.WriteLine(TimeSpan.FromSeconds(totalPictures + uploadedPictures)
+                    .ToString(new string('d', 1) + @"\:hh\:mm\:ss", CultureInfo.InvariantCulture));
+        }
+    }
+}
