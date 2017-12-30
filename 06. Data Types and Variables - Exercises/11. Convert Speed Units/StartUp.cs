6d9c4fad084b275d7e29d0944d21d0a4cd6477c3
Teodor <thelad43@gmail.com>
2017-12-30 15:52:50 +0200

    Added solution for problem 11. Convert Speed Units

---
+++ 06. Data Types and Variables - Exercises/11. Convert Speed Units/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _11.Convert_Speed_Units
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var distanceInMeters = float.Parse(Console.ReadLine());
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+            var seconds = int.Parse(Console.ReadLine());
+            var totalSeconds = (ushort)(hours * 3600 + minutes * 60 + seconds);
+
+            Console.WriteLine("{0:0.#######}", distanceInMeters / totalSeconds);
+            Console.WriteLine("{0:0.#######}", (distanceInMeters / 1000F) / (totalSeconds / 3600F));
+            Console.WriteLine("{0:0.#######}", (distanceInMeters / 1609F) / (totalSeconds / 3600F));
+        }
+    }
+}
