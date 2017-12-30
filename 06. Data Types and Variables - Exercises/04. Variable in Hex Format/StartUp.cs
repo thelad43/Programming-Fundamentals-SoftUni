56b89ccad353d271c88140de6d4b796c3a2d1428
Teodor <thelad43@gmail.com>
2017-12-30 15:44:20 +0200

    Added solution for problem 04. Variable in Hex Format

---
+++ 06. Data Types and Variables - Exercises/04. Variable in Hex Format/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.Variable_in_Hex_Format
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var hexadecimal = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(hexadecimal, 16));
+        }
+    }
+}
