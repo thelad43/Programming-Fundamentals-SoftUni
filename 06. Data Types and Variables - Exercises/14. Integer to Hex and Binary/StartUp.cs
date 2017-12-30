f687732b267b466e19157f9dfdfcac5d68a87920
Teodor <thelad43@gmail.com>
2017-12-30 15:55:32 +0200

    Added solution for problem 14. Integer to Hex and Binary

---
+++ 06. Data Types and Variables - Exercises/14. Integer to Hex and Binary/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _14.Integer_to_Hex_and_Binary
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var decimalNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(decimalNumber, 16).ToUpper());
+            Console.WriteLine(Convert.ToString(decimalNumber, 2));
+        }
+    }
+}
