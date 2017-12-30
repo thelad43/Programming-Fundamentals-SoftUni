e8b2c4fd9ba71b9be16dbf279983e57608af49cf
Teodor <thelad43@gmail.com>
2017-12-30 15:58:28 +0200

    Added solution for problem 17. Print Part Of ASCII Table

---
+++ 06. Data Types and Variables - Exercises/17. Print Part Of ASCII Table/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _17.Print_Part_Of_ASCII_Table
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var from = int.Parse(Console.ReadLine());
+            var to = int.Parse(Console.ReadLine());
+
+            for (int i = from; i <= to; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
