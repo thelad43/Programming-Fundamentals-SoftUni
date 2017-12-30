426e2168cbe4d354e05893817a6ac86aa30f04af
Teodor <thelad43@gmail.com>
2017-12-30 15:45:19 +0200

    Added solution for problem 05. Boolean Variable

---
+++ 06. Data Types and Variables - Exercises/05. Boolean Variable/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.Boolean_Variable
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            if (Convert.ToBoolean(input))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
