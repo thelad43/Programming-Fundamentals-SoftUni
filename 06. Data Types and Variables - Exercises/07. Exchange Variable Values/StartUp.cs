6d690353e600351cab46c7fcaf4a8763e3e3b98b
Teodor <thelad43@gmail.com>
2017-12-30 15:48:29 +0200

    Added solution for problem 07. Exchange Variable Values

---
+++ 06. Data Types and Variables - Exercises/07. Exchange Variable Values/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.Exchange_Variable_Values
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = 5;
+            var b = 10;
+
+            var oldValue = a;
+            a = b;
+            b = oldValue;
+
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = 5");
+            Console.WriteLine($"b = 10");
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
