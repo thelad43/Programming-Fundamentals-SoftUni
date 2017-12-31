90e4de3da967cc0b6adeba536b126be6d2481d15
Teodor <thelad43@gmail.com>
2017-12-31 15:10:00 +0200

    Added solution for problem 03. Printing Triangle

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/03. Printing Triangle/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace _03.Printing_Triangle
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        private static void PrintTriangle(int n)
+        {
+            UpperPartOfTriangle(n);
+            MiddlePartOfTriangle(n);
+            LowerPartOfTriangle(n);
+        }
+
+        private static void UpperPartOfTriangle(int n)
+        {
+            for (int i = 1; i <= n - 1; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("{0} ", k);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void MiddlePartOfTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void LowerPartOfTriangle(int n)
+        {
+            for (int i = 1; i <= n - 1; i++)
+            {
+                for (int k = 1; k <= n - 1 - i + 1; k++)
+                {
+                    Console.Write("{0} ", k);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
