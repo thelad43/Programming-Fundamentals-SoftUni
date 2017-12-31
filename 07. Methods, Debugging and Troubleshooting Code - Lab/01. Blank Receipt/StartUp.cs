33b3fac85c8c5d9bc71422b64d294734f9c4f545
Teodor <thelad43@gmail.com>
2017-12-31 15:07:50 +0200

    Added solution for problem 01. Blank Receipt

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/01. Blank Receipt/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Blank_Receipt
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            PrintBlankCashReceipt();
+        }
+
+        private static void PrintBlankCashReceipt()
+        {
+            PrintingHeader();
+            PrintBody();
+            PrintFooter();
+        }
+
+        private static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+
+        private static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        private static void PrintingHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
