db592cb291b39c7371a8bff2d167e5e667c38d48
Teodor <thelad43@gmail.com>
2017-12-31 15:08:50 +0200

    Added solution for problem 02. Sign of Integer Number

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/02. Sign of Integer Number/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.Sign_of_Integer_Number
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+            PrintSign(num);
+        }
+
+        private static void PrintSign(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+        }
+    }
+}
