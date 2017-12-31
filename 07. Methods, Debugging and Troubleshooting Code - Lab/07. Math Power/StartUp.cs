8f5266581b4c776bb3a5c16965a6be9c55e1d6da
Teodor <thelad43@gmail.com>
2017-12-31 15:14:29 +0200

    Added solution for problem 07. Math Power

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/07. Math Power/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.Math_Power
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+            var result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        private static double RaiseToPower(double number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
