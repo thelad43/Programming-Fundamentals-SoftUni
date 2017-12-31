a897e2343de5e58820452a26a461ea2d1550a394
Teodor <thelad43@gmail.com>
2017-12-31 15:12:41 +0200

    Added solution for problem 05. Temperature Conversion

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/05. Temperature Conversion/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.Temperature_Conversion
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var fahrenheitTemp = double.Parse(Console.ReadLine());
+            var celsius = FahrenheitToCelsius(fahrenheitTemp);
+            Console.WriteLine($"{celsius:F2}");
+        }
+
+        private static double FahrenheitToCelsius(double fahrenheitTemp)
+        {
+            return (fahrenheitTemp - 32) * 5 / 9;
+        }
+    }
+}
