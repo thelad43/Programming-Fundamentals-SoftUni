7e308eb5ef9f7e28d8afe63f1223a6bedd9f2d34
Teodor <thelad43@gmail.com>
2017-12-31 15:13:36 +0200

    Added solution for problem 06. Calculate Triangle Area

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/06. Calculate Triangle Area/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.Calculate_Triangle_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var baseTriangle = double.Parse(Console.ReadLine());
+            var heightTriangle = double.Parse(Console.ReadLine());
+            var area = GetTriangleArea(baseTriangle, heightTriangle);
+            Console.WriteLine(area);
+        }
+
+        private static double GetTriangleArea(double baseTriangle, double heightTriangle)
+        {
+            return baseTriangle * heightTriangle / 2;
+        }
+    }
+}
