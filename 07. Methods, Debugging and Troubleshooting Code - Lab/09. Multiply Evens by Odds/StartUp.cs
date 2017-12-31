2450fe8a80b6d1590fcc8883a9ca15e8a507e356
Teodor <thelad43@gmail.com>
2017-12-31 15:16:14 +0200

    Added solution for problem 09. Multiply Evens by Odds

---
+++ 07. Methods, Debugging and Troubleshooting Code - Lab/09. Multiply Evens by Odds/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace _09.Multiply_Evens_by_Odds
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var number = Math.Abs(int.Parse(Console.ReadLine()));
+            var result = MultipliesEvensToOdds(number);
+            Console.WriteLine(result);
+        }
+
+        private static int GetTheSumOfEvenDigits(int number)
+        {
+            var evenSum = 0;
+
+            while (number > 0)
+            {
+                var lastDigit = number % 10;
+                number = number / 10;
+
+                if (lastDigit % 2 == 0)
+                {
+                    evenSum += lastDigit;
+                }
+            }
+
+            return evenSum;
+        }
+
+        private static int GetTheSumOfOddDigits(int number)
+        {
+            var oddSum = 0;
+
+            while (number > 0)
+            {
+                var lastDigit = number % 10;
+                number = number / 10;
+
+                if (lastDigit % 2 != 0)
+                {
+                    oddSum += lastDigit;
+                }
+            }
+
+            return oddSum;
+        }
+
+        private static int MultipliesEvensToOdds(int number)
+        {
+            var evenSum = GetTheSumOfEvenDigits(number);
+            var oddSum = GetTheSumOfOddDigits(number);
+
+            return evenSum * oddSum;
+        }
+    }
+}
