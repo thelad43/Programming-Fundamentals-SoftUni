1e276dbb512bb123c0f417e2af693d0ade01d62f
Teodor <thelad43@gmail.com>
2018-01-04 16:38:29 +0200

    Added solution for problem 12. Master Number

--- 08. Methods, Debugging and Troubleshooting Code - Exercises/12. Master Number/StartUp.cs
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/12. Master Number/StartUp.cs
@@ -1,27 +1,78 @@
-﻿namespace _13.Factorial
+﻿namespace _12.Master_Number
 {
     using System;
-    using System.Numerics;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
             var n = int.Parse(Console.ReadLine());
-            var result = CalculateFactorial(n);
-            Console.WriteLine(result);
-        }
-
-        private static BigInteger CalculateFactorial(int n)
-        {
-            BigInteger factorial = 1;
 
             for (int i = 1; i <= n; i++)
             {
-                factorial = factorial * i;
+                if (IsPalindrome(i) && IsSumOfDigitsDivisibleBySeven(i) && ContainsEvenDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool ContainsEvenDigit(int n)
+        {
+            if (n == 0)
+                return true;
+
+            while (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    return true;
+                }
+
+                n /= 10;
             }
 
-            return factorial;
+            return false;
+        }
+
+        private static bool IsSumOfDigitsDivisibleBySeven(int n)
+        {
+            var digits = Array.ConvertAll(n.ToString().ToArray(), x => x - 48);
+            var sum = 0;
+
+            foreach (var digit in digits)
+            {
+                sum += digit;
+            }
+
+            if (sum % 7 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPalindrome(int n)
+        {
+            var remainder = 0;
+            var oldValue = n;
+            var reversed = 0;
+
+            while (n > 0)
+            {
+                remainder = n % 10;
+                reversed = reversed * 10 + remainder;
+                n /= 10;
+            }
+
+            if (oldValue == reversed)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
