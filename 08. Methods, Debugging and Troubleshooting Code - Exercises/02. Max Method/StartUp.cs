652e31616846aa0d93065d4846952279ae75c264
Teodor <thelad43@gmail.com>
2018-01-04 16:21:03 +0200

    Added solution for problem 02. Max Method

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/02. Max Method/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.Max_Method
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMax(a, GetMax(b, c)));
+        }
+
+        private static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+
+            return b;
+        }
+    }
+}
