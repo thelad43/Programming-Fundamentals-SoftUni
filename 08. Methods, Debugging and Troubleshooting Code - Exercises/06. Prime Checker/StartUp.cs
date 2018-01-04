99f346d035a40dd08b7c60ea0da48ef7395cee8e
Teodor <thelad43@gmail.com>
2018-01-04 16:26:15 +0200

    Added solution for problem 06. Prime Checker

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/06. Prime Checker/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.Prime_Checker
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+            var isPrime = IsPrime(n);
+            Console.WriteLine(isPrime);
+        }
+
+        private static bool IsPrime(long number)
+        {
+            if (number == 1)
+                return false;
+            if (number == 2)
+                return true;
+            if (number % 2 == 0)
+                return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
