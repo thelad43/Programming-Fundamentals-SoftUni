27ea8e9201f980aa3900fa03251470164c96fc5a
Teodor <thelad43@gmail.com>
2018-01-04 16:19:48 +0200

    Added solution for problem 01. Hello, Name!

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/01. Hello, Name!/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.Hello__Name_
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            PrintName(name);
+        }
+
+        private static void PrintName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
