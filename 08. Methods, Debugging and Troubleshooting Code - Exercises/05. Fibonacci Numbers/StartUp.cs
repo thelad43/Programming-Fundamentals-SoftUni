d5c19e3ff461e29048c3db4983f9239fcd673854
Teodor <thelad43@gmail.com>
2018-01-04 16:25:07 +0200

    Added solution for problem 05. Fibonacci Numbers

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/05. Fibonacci Numbers/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.Fibonacci_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = GetFibonacciNumber(n);
+            Console.WriteLine(result);
+        }
+
+        private static int GetFibonacciNumber(int n)
+        {
+            var first = 1;
+            var second = 1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                var third = first + second;
+                first = second;
+                second = third;
+            }
+
+            return second;
+        }
+    }
+}
