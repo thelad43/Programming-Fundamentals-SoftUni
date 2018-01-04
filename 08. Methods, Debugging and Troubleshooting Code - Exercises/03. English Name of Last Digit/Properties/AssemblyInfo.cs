48d7c46d6950bcee0258b8dc147e0113e693db4b
Teodor <thelad43@gmail.com>
2018-01-04 16:22:03 +0200

    Added solution for problem 03. English Name of Last Digit

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/03. English Name of Last Digit/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("03. English Name of Last Digit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("03. English Name of Last Digit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("369a3c11-3e2f-4b73-87a4-e8b0b10b9592")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/03. English Name of Last Digit/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace _03.English_Name_of_Last_Digit
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = long.Parse(Console.ReadLine());
+            var lastDigit = num % 10;
+            PrintEnglishNameOfLastDigit(Math.Abs(lastDigit));
+        }
+
+        private static void PrintEnglishNameOfLastDigit(long lastDigit)
+        {
+            switch (lastDigit)
+            {
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
