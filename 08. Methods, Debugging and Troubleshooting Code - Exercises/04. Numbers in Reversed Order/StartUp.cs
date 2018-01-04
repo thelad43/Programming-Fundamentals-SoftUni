8f73e8ca4ca7faafcf8283aa916f157fb541adc1
Teodor <thelad43@gmail.com>
2018-01-04 16:23:05 +0200

    Added solution for problem 04. Numbers in Reversed Order

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/04. Numbers in Reversed Order/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.Numbers_in_Reversed_Order
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = double.Parse(Console.ReadLine());
+            var result = ReverseNum(num);
+            Console.WriteLine(result);
+        }
+
+        private static string ReverseNum(double num)
+        {
+            var digits = num.ToString().ToCharArray();
+            Array.Reverse(digits);
+            var result = string.Join("", digits);
+            return result;
+        }
+    }
+}
