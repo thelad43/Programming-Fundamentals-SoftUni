841636d75308574844677c8b4287718789ecca0e
Teodor <thelad43@gmail.com>
2018-01-04 16:45:07 +0200

    Added solution for problem 17. ** Be Positive

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/17. Be Positive/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace _17.Be_Positive
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var sequences = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sequences; i++)
+            {
+                var numbersToPrint = new List<int>();
+
+                var numbers = Console.ReadLine()
+                    .Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    var currentNum = numbers[j];
+
+                    if (currentNum >= 0)
+                    {
+                        numbersToPrint.Add(currentNum);
+                    }
+                    else
+                    {
+                        if (j == numbers.Length - 1)
+                        {
+                            break;
+                        }
+
+                        currentNum += numbers[j + 1];
+
+                        if (currentNum >= 0)
+                        {
+                            numbersToPrint.Add(currentNum);
+                        }
+
+                        j++;
+                    }
+                }
+
+                if (numbersToPrint.Count > 0)
+                {
+                    Console.WriteLine(string.Join(" ", numbersToPrint));
+                }
+                else
+                {
+                    Console.WriteLine("(empty)");
+                }
+            }
+        }
+    }
+}
