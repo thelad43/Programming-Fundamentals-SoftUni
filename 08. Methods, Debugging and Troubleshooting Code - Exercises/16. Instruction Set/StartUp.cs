dae6ed11caa6dc1b2024083b0848977f1ee85bb7
Teodor <thelad43@gmail.com>
2018-01-04 16:44:00 +0200

    Added solution for problem 16. ** Instruction Set

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/16. Instruction Set/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace _16.Instruction_Set
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var opCode = Console.ReadLine().ToUpper();
+            long result = 0;
+
+            while (opCode != "END")
+            {
+                var codeArgs = opCode.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            var operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne + 1;
+                            break;
+                        }
+
+                    case "DEC":
+                        {
+                            var operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne - 1;
+                            break;
+                        }
+
+                    case "ADD":
+                        {
+                            var operandOne = long.Parse(codeArgs[1]);
+                            var operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+
+                    case "MLA":
+                        {
+                            var operandOne = long.Parse(codeArgs[1]);
+                            var operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne * operandTwo;
+                            break;
+                        }
+                }
+
+                Console.WriteLine(result);
+                opCode = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
