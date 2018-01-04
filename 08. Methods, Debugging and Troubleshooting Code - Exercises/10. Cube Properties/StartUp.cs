8384a81f4ce30d4d8ffc3cd20295d7eb91d6fef5
Teodor <thelad43@gmail.com>
2018-01-04 16:30:32 +0200

    Added solution for problem 10. Cube Properties

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/10. Cube Properties/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace _10.Cube_Properties
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var side = double.Parse(Console.ReadLine());
+            var parameter = Console.ReadLine().ToLower();
+            var result = 0D;
+
+            switch (parameter)
+            {
+                case "face":
+                    result = GetFaceDiagonals(side);
+                    break;
+
+                case "space":
+                    result = GetSpaceDiagonals(side);
+                    break;
+
+                case "volume":
+                    result = GetVolume(side);
+                    break;
+
+                case "area":
+                    result = GetSurfaceArea(side);
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            Console.WriteLine($"{result:F2}");
+        }
+
+        private static double GetSurfaceArea(double side)
+        {
+            return 6 * (side * side);
+        }
+
+        private static double GetVolume(double side)
+        {
+            return side * side * side;
+        }
+
+        private static double GetSpaceDiagonals(double side)
+        {
+            return Math.Sqrt(3 * (side * side));
+        }
+
+        private static double GetFaceDiagonals(double side)
+        {
+            return Math.Sqrt(2 * (side * side));
+        }
+    }
+}
