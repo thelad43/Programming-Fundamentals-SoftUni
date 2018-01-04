65a9b9843b1470aad9971bfd0d5dea617c3300a4
Teodor <thelad43@gmail.com>
2018-01-04 16:40:02 +0200

    Added solution for problem 13. Factorial

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/13. Factorial/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _13.Factorial
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = CalculateFactorial(n);
+            Console.WriteLine(result);
+        }
+
+        private static BigInteger CalculateFactorial(int n)
+        {
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+    }
+}
