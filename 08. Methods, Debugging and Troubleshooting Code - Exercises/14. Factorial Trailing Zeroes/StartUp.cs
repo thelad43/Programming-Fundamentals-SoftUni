4085d3cece1512748038c55e8a794211a225a62a
Teodor <thelad43@gmail.com>
2018-01-04 16:41:44 +0200

    Added solution for problem 14. Factorial Trailing Zeroes

---
+++ 08. Methods, Debugging and Troubleshooting Code - Exercises/14. Factorial Trailing Zeroes/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace _14.Factorial_Trailing_Zeroes
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var factorial = CalculateFactorial(n);
+            var trailingZeroes = CalculateTrailingZeroes(factorial);
+            Console.WriteLine(trailingZeroes);
+        }
+
+        private static int CalculateTrailingZeroes(BigInteger factorial)
+        {
+            var factorialReversed = factorial.ToString().ToCharArray();
+            Array.Reverse(factorialReversed);
+            var count = 0;
+
+            foreach (var num in factorialReversed)
+            {
+                if (num == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                    return count;
+                }
+            }
+
+            return count;
+        }
+
+        private static BigInteger CalculateFactorial(int n)
+        {
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+    }
+}
