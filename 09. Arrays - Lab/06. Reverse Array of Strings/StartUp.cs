c1d0a338d65ff3138a0b47c4513a6ad412801830
Teodor <thelad43@gmail.com>
2018-01-06 14:08:31 +0200

    Added solution for problem 06. Reverse Array of Strings

---
+++ 09. Arrays - Lab/06. Reverse Array of Strings/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _06.Reverse_Array_of_Strings
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var arrayOfStrings = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Reverse(arrayOfStrings);
+            Console.WriteLine(string.Join(" ", arrayOfStrings));
+        }
+    }
+}
