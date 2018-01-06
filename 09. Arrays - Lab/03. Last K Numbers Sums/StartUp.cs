594fbb7cec27d1bac48bccd03b3f7dfdd611fbe8
Teodor <thelad43@gmail.com>
2018-01-06 14:04:50 +0200

    Added solution for problem 03. Last K Numbers Sums

---
+++ 09. Arrays - Lab/03. Last K Numbers Sums/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.Last_K_Numbers_Sums
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var nums = new long[n];
+            nums[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                long sum = 0;
+
+                for (int j = i - k; j <= i - 1; j++)
+                {
+                    if (j >= 0)
+                    {
+                        sum += nums[j];
+                    }
+                }
+
+                nums[i] = sum;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+        }
+    }
+}
