6da388c056fa12890cccbc57f1332002be9eb086
Teodor <thelad43@gmail.com>
2018-01-06 14:10:24 +0200

    Added solution for problem 08. Condense Array to Number

---
+++ 09. Arrays - Lab/08. Condense Array to Number/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.Condense_Array_to_Number
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            while (numbers.Length > 1)
+            {
+                var condensed = new int[numbers.Length - 1];
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                }
+
+                numbers = condensed;
+            }
+
+            Console.WriteLine(string.Join("", numbers));
+        }
+    }
+}
