1ac0ab89c90435e33a20780d1808327ac7cd0239
Teodor <thelad43@gmail.com>
2018-01-06 14:06:06 +0200

    Added solution for problem 04. Triple Sum

---
+++ 09. Arrays - Lab/04. Triple Sum/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.Triple_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var notFound = true;
+            var sum = 0;
+
+            for (int a = 0; a < numbers.Length; a++)
+            {
+                for (int b = a + 1; b < numbers.Length; b++)
+                {
+                    sum = numbers[a] + numbers[b];
+
+                    if (numbers.Contains(sum))
+                    {
+                        Console.WriteLine($"{numbers[a]} + {numbers[b]} == {sum}");
+
+                        if (notFound)
+                        {
+                            notFound = false;
+                        }
+                    }
+                }
+            }
+
+            if (notFound)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
