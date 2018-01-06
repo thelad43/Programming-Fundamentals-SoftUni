46091073f0a99562f259bdbbef3c3e6d204522d7
Teodor <thelad43@gmail.com>
2018-01-06 14:02:46 +0200

    Added solution for problem 01. Day of Week

---
+++ 09. Arrays - Lab/01. Day of Week/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.Day_of_Week
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var day = int.Parse(Console.ReadLine());
+            var days = new string[]
+            {
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+
+            try
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
