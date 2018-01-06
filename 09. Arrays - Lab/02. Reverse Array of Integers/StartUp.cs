f9a3b7cbbe691e2eff34a74b36d88c7fb51dcb72
Teodor <thelad43@gmail.com>
2018-01-06 14:03:53 +0200

    Added solution for problem 02. Reverse Array of Integers

---
+++ 09. Arrays - Lab/02. Reverse Array of Integers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.Reverse_Array_of_Integers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
