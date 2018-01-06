db5f9082f880454a8a70c13a5c85b1addfba5222
Teodor <thelad43@gmail.com>
2018-01-06 14:07:10 +0200

    Added solution for problem 05. Rounding Numbers

---
+++ 09. Arrays - Lab/05. Rounding Numbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.Rounding_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number} => {number:F0}");
+            }
+        }
+    }
+}
