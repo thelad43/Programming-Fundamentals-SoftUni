ad2759aad5cec6f4caef4ccd4687025bb1c6621f
Teodor <thelad43@gmail.com>
2018-01-08 18:20:01 +0200

    Added solution for problem 08. Most Frequent Number

---
+++ 10. Arrays - Exercises/08. Most Frequent Number/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _08.Most_Frequent_Number
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var mostFrequentElement = numbers.GroupBy(item => item).OrderByDescending(g => g.Count()).Select(g => g.Key).First();
+            Console.WriteLine(mostFrequentElement);
+        }
+    }
+}
