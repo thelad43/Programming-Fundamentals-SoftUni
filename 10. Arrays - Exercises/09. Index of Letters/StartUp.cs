10a5c47c69739cf1bfd057f827ecd45165c49970
Teodor <thelad43@gmail.com>
2018-01-08 18:20:59 +0200

    Added solution for problem 09. Index of Letters

---
+++ 10. Arrays - Exercises/09. Index of Letters/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _09.Index_of_Letters
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var word = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine("{0} -> {1}", word[i], word[i] - 'a');
+            }
+        }
+    }
+}
