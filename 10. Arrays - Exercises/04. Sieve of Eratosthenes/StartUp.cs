c3652837586b65d54ad5872de7050a6935454bac
Teodor <thelad43@gmail.com>
2018-01-08 18:15:34 +0200

    Added solution for problem 04. Sieve of Eratosthenes

---
+++ 10. Arrays - Exercises/04. Sieve of Eratosthenes/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _04.Sieve_of_Eratosthenes
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var primes = new bool[n + 1];
+
+            for (int num = 0; num <= n; num++)
+            {
+                primes[num] = true;
+            }
+
+            primes[0] = primes[1] = false;
+
+            for (int num = 1; num < primes.Length; num++)
+            {
+                if (primes[num] == true)
+                {
+                    for (int multiplier = 2; multiplier * num <= n; multiplier++)
+                    {
+                        primes[multiplier * num] = false;
+                    }
+                }
+            }
+
+            for (int num = 2; num <= n; num++)
+            {
+                if (primes[num] == true)
+                {
+                    Console.Write($"{num} ");
+                }
+            }
+        }
+    }
+}
