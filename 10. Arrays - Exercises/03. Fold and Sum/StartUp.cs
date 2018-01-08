fdcdf77e9f59ecf5d176b42e15dff28c00a7ea7b
Teodor <thelad43@gmail.com>
2018-01-08 18:14:36 +0200

    Added solution for problem 03. Fold and Sum

---
+++ 10. Arrays - Exercises/03. Fold and Sum/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Fold_and_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var k = numbers.Length / 4;
+            var rowLeft = numbers.Take(k).Reverse();
+            var rowRight = numbers.Reverse().Take(k);
+            var firstRow = rowLeft.Concat(rowRight).ToArray();
+            var secondRow = numbers.Skip(k).Take(2 * k).ToArray();
+            var sumArr = firstRow.Select((x, index) => x + secondRow[index]);
+            Console.WriteLine(string.Join(" ", sumArr));
+        }
+    }
+}
