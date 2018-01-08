b28624659a7b0f30dd1344c0eaaa97360ba4afde
Teodor <thelad43@gmail.com>
2018-01-08 18:16:45 +0200

    Added solution for problem 05. Compare Char Arrays

---
+++ 10. Arrays - Exercises/05. Compare Char Arrays/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.Compare_Char_Arrays
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstCharArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+            var secondCharArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+            if (firstCharArray.Length == secondCharArray.Length)
+            {
+                if (firstCharArray[0] < secondCharArray[0])
+                {
+                    Console.WriteLine(firstCharArray);
+                    Console.WriteLine(secondCharArray);
+                }
+                else
+                {
+                    Console.WriteLine(secondCharArray);
+                    Console.WriteLine(firstCharArray);
+                }
+            }
+            else if (firstCharArray.Length > secondCharArray.Length)
+            {
+                Console.WriteLine(secondCharArray);
+                Console.WriteLine(firstCharArray);
+            }
+            else
+            {
+                Console.WriteLine(firstCharArray);
+                Console.WriteLine(secondCharArray);
+            }
+        }
+    }
+}
