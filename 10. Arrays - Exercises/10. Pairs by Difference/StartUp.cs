35c51e12374f07896d891dd94f401f898d36b1ed
Teodor <thelad43@gmail.com>
2018-01-08 18:22:06 +0200

    Added solution for problem 10. Pairs by Difference

---
+++ 10. Arrays - Exercises/10. Pairs by Difference/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _10.Pairs_by_Difference
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var difference = int.Parse(Console.ReadLine());
+            var count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (Math.Abs(numbers[i] - numbers[j]) == difference)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
