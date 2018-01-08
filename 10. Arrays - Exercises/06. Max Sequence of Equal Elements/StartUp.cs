6c72c5accd28f7c7016d2f1703bd77d32a3aa228
Teodor <thelad43@gmail.com>
2018-01-08 18:18:02 +0200

    Added solution for problem 06. Max Sequence of Equal Elements

---
+++ 10. Arrays - Exercises/06. Max Sequence of Equal Elements/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.Max_Sequence_of_Equal_Elements
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var start = 0;
+            var bestStart = 0;
+            var length = 0;
+            var bestLength = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[start] == numbers[i])
+                {
+                    length++;
+
+                    if (length > bestLength)
+                    {
+                        bestLength = length;
+                        bestStart = start;
+                    }
+                }
+                else
+                {
+                    start++;
+                    i = start;
+                    length = 0;
+                }
+            }
+
+            for (int i = 0; i <= bestLength; i++)
+            {
+                Console.Write($"{numbers[bestStart + i]} ");
+            }
+        }
+    }
+}
