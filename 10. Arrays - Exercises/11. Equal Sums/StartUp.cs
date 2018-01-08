068c0d8daf1ca92f1632246c2c82348228299bc3
Teodor <thelad43@gmail.com>
2018-01-08 18:23:17 +0200

    Added solution for problem 11. Equal Sums

---
+++ 10. Arrays - Exercises/11. Equal Sums/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace _11.Equal_Sums
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var leftSum = 0;
+            var rightSum = 0;
+            var hasMatch = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                leftSum = CalculateLeftSum(numbers, i);
+                rightSum = CalculateRightSum(numbers, i);
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    hasMatch = true;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+
+        private static int CalculateRightSum(int[] numbers, int i)
+        {
+            var numbersInRight = numbers.Skip(i + 1).ToArray();
+            return numbersInRight.Sum();
+        }
+
+        private static int CalculateLeftSum(int[] numbers, int i)
+        {
+            var numbersInLeft = numbers.Take(i).ToArray();
+            return numbersInLeft.Sum();
+        }
+    }
+}
