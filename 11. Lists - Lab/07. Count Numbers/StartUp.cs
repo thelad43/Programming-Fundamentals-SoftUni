31ae7010f92668d25c2e383cb6c7090a1d6e85d7
Teodor <thelad43@gmail.com>
2018-01-10 13:57:01 +0200

    Added solution for problem 07. Count Numbers

---
+++ 11. Lists - Lab/07. Count Numbers/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _07.Count_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+              .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToList();
+
+            var numbersAndOccurrences = new Dictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!numbersAndOccurrences.ContainsKey(number))
+                {
+                    numbersAndOccurrences[number] = 1;
+                }
+                else
+                {
+                    numbersAndOccurrences[number]++;
+                }
+            }
+
+            foreach (var kvp in numbersAndOccurrences.OrderBy(n => n.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
