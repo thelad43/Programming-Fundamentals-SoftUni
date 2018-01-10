3f9219e23fad2809e1524ba0eac93fec893eee77
Teodor <thelad43@gmail.com>
2018-01-10 13:54:22 +0200

    Added solution for problem 05. Sort Numbers

---
+++ 11. Lists - Lab/05. Sort Numbers/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.Sort_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .OrderBy(n => n)
+                .ToList();
+
+            var result = new List<string>(numbers.Count);
+            int i;
+
+            for (i = 0; i < numbers.Count - 1; i++)
+            {
+                result.Add($"{numbers[i]} <= ");
+            }
+
+            result.Add(string.Empty + numbers[i]);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
