2cb81b6995b65818f9b120c9bd207d71f275862a
Teodor <thelad43@gmail.com>
2018-01-10 13:49:15 +0200

    Added solution for problem 01. Remove Negatives and Reverse

---
+++ 11. Lists - Lab/01. Remove Negatives and Reverse/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Remove_Negatives_and_Reverse
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n > 0)
+                .Reverse()
+                .ToList();
+
+            var result = numbers.Count != 0 ? string.Join(" ", numbers) : "empty";
+            Console.WriteLine(result);
+        }
+    }
+}
