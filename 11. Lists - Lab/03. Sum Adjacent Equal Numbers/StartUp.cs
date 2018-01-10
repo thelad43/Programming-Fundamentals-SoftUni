f144b3245d9fd12fe885475e401f3810dc0c3560
Teodor <thelad43@gmail.com>
2018-01-10 13:52:13 +0200

    Added solution for problem 03. Sum Adjacent Equal Numbers

---
+++ 11. Lists - Lab/03. Sum Adjacent Equal Numbers/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.Sum_Adjacent_Equal_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            SumAdjacentEqualNumbers(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SumAdjacentEqualNumbers(List<double> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    var sum = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i);
+                    numbers[i] = sum;
+                    SumAdjacentEqualNumbers(numbers);
+                }
+            }
+        }
+    }
+}
