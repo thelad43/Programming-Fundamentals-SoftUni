9b4ca20740be996df3d05b257e4e139f821be8ea
Teodor <thelad43@gmail.com>
2018-01-10 13:55:56 +0200

    Added solution for problem 06. Square Numbers

---
+++ 11. Lists - Lab/06. Square Numbers/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.Square_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            var squareNumbers = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (Math.Sqrt(number) == (int)Math.Sqrt(number))
+                {
+                    squareNumbers.Add(number);
+                }
+            }
+
+            squareNumbers.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(string.Join(" ", squareNumbers));
+        }
+    }
+}
