2b9ce32e65d508d2152f40a2f5c36b3ea2e241b6
Teodor <thelad43@gmail.com>
2018-01-10 13:51:00 +0200

    Added solution for problem 02. Append Lists

---
+++ 11. Lists - Lab/02. Append Lists/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.Append_Lists
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+
+            foreach (var list in tokens)
+            {
+                var numbers = list
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                Console.Write(string.Join(" ", numbers) + " ");
+            }
+        }
+    }
+}
