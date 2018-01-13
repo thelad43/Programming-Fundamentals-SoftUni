92f53838879153b61fd47ba4e2542d34837f5997
Teodor <thelad43@gmail.com>
2018-01-13 13:17:22 +0200

    Added solution for problem 02. Change List

---
+++ 12. Lists - Exercises/02. Change List/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace _02.Change_List
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                  .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse)
+                  .ToList();
+
+            var cmd = Console.ReadLine();
+
+            while (cmd != "Odd" && cmd != "Even")
+            {
+                var tokens = cmd.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var command = tokens[0];
+
+                if (command == "Delete")
+                {
+                    var element = int.Parse(tokens[1]);
+                    numbers.RemoveAll(n => n == element);
+                }
+                else
+                {
+                    var element = int.Parse(tokens[1]);
+                    var position = int.Parse(tokens[2]);
+                    numbers.Insert(position, element);
+                }
+
+                cmd = Console.ReadLine();
+            }
+
+            if (cmd == "Even")
+            {
+                numbers
+                    .Where(n => n % 2 == 0)
+                    .ToList()
+                    .ForEach(n => Console.Write($"{n} "));
+            }
+            else
+            {
+                numbers
+                   .Where(n => n % 2 == 1)
+                   .ToList()
+                   .ForEach(n => Console.Write($"{n} "));
+            }
+        }
+    }
+}
