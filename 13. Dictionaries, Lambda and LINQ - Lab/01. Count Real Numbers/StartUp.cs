d4a53c29ebafb364f191d313c12a650bed16f503
Teodor <thelad43@gmail.com>
2018-01-13 17:24:50 +0200

    Added solution for problem 01. Count Real Numbers

---
+++ 13. Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.Count_Real_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!counts.ContainsKey(numbers[i]))
+                {
+                    counts[numbers[i]] = 1;
+                    continue;
+                }
+
+                counts[numbers[i]]++;
+            }
+
+            foreach (var kvp in counts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
