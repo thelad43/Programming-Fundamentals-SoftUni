75f32b53aeeb45009027682f085b9e87c855a997
Teodor <thelad43@gmail.com>
2018-01-13 13:18:11 +0200

    Added solution for problem 03. Search for a Number

---
+++ 12. Lists - Exercises/03. Search for a Number/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.Search_for_a_Number
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var numbersForOperations = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            var manipulationNumbers = numbers
+                .Take(numbersForOperations[0])
+                .ToList();
+
+            manipulationNumbers.RemoveRange(0, numbersForOperations[1]);
+
+            if (manipulationNumbers.Contains(numbersForOperations[2]))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
