fede7d8920f7107ac0b17002f50678440faf4845
Teodor <thelad43@gmail.com>
2018-01-13 13:16:31 +0200

    Added solution for problem 01. Max Sequence of Equal Elements

---
+++ 12. Lists - Exercises/01. Max Sequence of Equal Elements/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("01. Max Sequence of Equal Elements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01. Max Sequence of Equal Elements")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a3fb6be-9ded-45d3-8bf8-444e125cba4c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 12. Lists - Exercises/01. Max Sequence of Equal Elements/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace _01.Max_Sequence_of_Equal_Elements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var bestSequance = GetLongestSequence(numbers);
+            Console.WriteLine(string.Join(" ", bestSequance));
+        }
+
+        private static List<int> GetLongestSequence(List<int> numbers)
+        {
+            var maxLength = 1;
+            var currentLength = 1;
+            var maxIndex = 0;
+            var currentIndex = 0;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    currentIndex = i;
+                    currentLength = 1;
+                }
+
+                if (maxLength < currentLength)
+                {
+                    maxLength = currentLength;
+                    maxIndex = currentIndex;
+                }
+            }
+
+            var longestSequance = new List<int>();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSequance.Add(numbers[maxIndex + i]);
+            }
+
+            return longestSequance;
+        }
+    }
+}
