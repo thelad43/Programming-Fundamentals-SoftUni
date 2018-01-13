f45dfac237839d626dd24dd4cfc69e45d1916abb
Teodor <thelad43@gmail.com>
2018-01-13 17:29:12 +0200

    Added solution for problem 05. Short Words Sorted

---
+++ 13. Dictionaries, Lambda and LINQ - Lab/05. Short Words Sorted/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.Short_Words_Sorted
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(".,:;()[]\"'\\/!? ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => word.Length < 5)
+                .OrderBy(word => word)
+                .Distinct()
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", words));
+        }
+    }
+}
