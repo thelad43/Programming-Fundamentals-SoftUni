2219a6c1c5be379abb417c210e701ec647f508aa
Teodor <thelad43@gmail.com>
2018-01-13 17:25:43 +0200

    Added solution for problem 02. Odd Occurrences

---
+++ 13. Dictionaries, Lambda and LINQ - Lab/02. Odd Occurrences/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.Odd_Occurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var countOfOccurrences = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!countOfOccurrences.ContainsKey(words[i]))
+                {
+                    countOfOccurrences[words[i]] = 1;
+                    continue;
+                }
+
+                countOfOccurrences[words[i]]++;
+            }
+
+            var result = new StringBuilder();
+
+            foreach (var kvp in countOfOccurrences)
+            {
+                if (kvp.Value % 2 == 1)
+                {
+                    result.Append($"{kvp.Key}, ");
+                }
+            }
+
+            Console.WriteLine(result.ToString().Trim(' ', ','));
+        }
+    }
+}
