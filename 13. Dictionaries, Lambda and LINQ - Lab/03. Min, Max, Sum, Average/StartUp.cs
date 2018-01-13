a0d41d642d0cecdf80dda67eba179d10556baf65
Teodor <thelad43@gmail.com>
2018-01-13 17:27:05 +0200

    Added solution for problem 03. Min, Max, Sum, Average

---
+++ 13. Dictionaries, Lambda and LINQ - Lab/03. Min, Max, Sum, Average/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Min_Max_Sum_Average
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(value: $"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Min = {numbers.Min()}");
+            Console.WriteLine($"Max = {numbers.Max()}");
+            Console.WriteLine($"Average = {numbers.Average()}");
+        }
+    }
+}
