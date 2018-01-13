cfc184c310c7278153bd2402650bb48d2d85bf56
Teodor <thelad43@gmail.com>
2018-01-13 17:28:07 +0200

    Added solution for problem 04. Largest 3 Numbers

---
+++ 13. Dictionaries, Lambda and LINQ - Lab/04. Largest 3 Numbers/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Largest_3_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(string.Join(" ", Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .OrderByDescending(n => n)
+                .Take(3)));
+        }
+    }
+}
