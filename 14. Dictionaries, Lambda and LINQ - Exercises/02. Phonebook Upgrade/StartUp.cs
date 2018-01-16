813630edeb211712de4ceb99d44577f0c3f568df
Teodor <thelad43@gmail.com>
2018-01-16 16:58:53 +0200

    Added solution for problem 02. Phonebook Upgrade

---
+++ 14. Dictionaries, Lambda and LINQ - Exercises/02. Phonebook Upgrade/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace _02.Phonebook_Upgrade
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var sortedPhoneBook = new SortedDictionary<string, string>();
+
+            while (inputLine != "END")
+            {
+                var inputParams = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var command = inputParams[0];
+
+                if (command == "A")
+                {
+                    var name = inputParams[1];
+                    var phone = inputParams[2];
+                    sortedPhoneBook[name] = phone;
+                }
+                else if (command == "S")
+                {
+                    var name = inputParams[1];
+
+                    if (sortedPhoneBook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {sortedPhoneBook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                else if (command == "ListAll")
+                {
+                    foreach (var kvp in sortedPhoneBook)
+                    {
+                        Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
