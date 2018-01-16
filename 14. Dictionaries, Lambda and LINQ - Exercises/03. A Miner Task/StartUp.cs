4599ea52d364f83190593e81f88196844c0728b6
Teodor <thelad43@gmail.com>
2018-01-16 16:59:49 +0200

    Added solution for problem 03. A Miner Task

---
+++ 14. Dictionaries, Lambda and LINQ - Exercises/03. A Miner Task/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _03.A_Miner_Task
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var resource = string.Empty;
+            var quantity = 0L;
+            var resourcesAndQuantity = new Dictionary<string, long>();
+
+            while (true)
+            {
+                resource = Console.ReadLine();
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                quantity = long.Parse(Console.ReadLine());
+
+                if (resourcesAndQuantity.ContainsKey(resource))
+                {
+                    var valuePair = resourcesAndQuantity[resource];
+                    resourcesAndQuantity[resource] = valuePair + quantity;
+                }
+                else
+                {
+                    resourcesAndQuantity[resource] = quantity;
+                }
+            }
+
+            foreach (var kvp in resourcesAndQuantity)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
