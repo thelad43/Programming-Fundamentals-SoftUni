2e0d30eabd1adaa0f9146838ce5df5ea74df69f8
Teodor <thelad43@gmail.com>
2018-01-16 16:57:58 +0200

    Added solution for problem 01. Phonebook

---
+++ 14. Dictionaries, Lambda and LINQ - Exercises/01. Phonebook/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace _01.Phonebook
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var phoneBook = new Dictionary<string, string>();
+
+            while (inputLine != "END")
+            {
+                var inputParams = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var command = inputParams[0];
+                var name = inputParams[1];
+
+                if (command == "A")
+                {
+                    var phone = inputParams[2];
+                    phoneBook[name] = phone;
+                }
+                else if (command == "S")
+                {
+                    if (phoneBook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phoneBook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
