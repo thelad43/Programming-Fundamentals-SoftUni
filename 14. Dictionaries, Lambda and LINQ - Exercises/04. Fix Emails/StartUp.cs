c97292c096bf2de0e0c0b5223707cab727a820a9
Teodor <thelad43@gmail.com>
2018-01-16 17:00:52 +0200

    Added solution for problem 04. Fix Emails

---
+++ 14. Dictionaries, Lambda and LINQ - Exercises/04. Fix Emails/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.Fix_Emails
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var namesAndEmails = new Dictionary<string, string>();
+
+            while (true)
+            {
+                var name = Console.ReadLine();
+
+                if (name == "stop")
+                {
+                    break;
+                }
+
+                var email = Console.ReadLine();
+
+                if (!(email.EndsWith("us") || email.EndsWith("uk")))
+                {
+                    namesAndEmails[name] = email;
+                }
+            }
+
+            foreach (var kvp in namesAndEmails)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
