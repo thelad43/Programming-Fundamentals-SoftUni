a7b29d15d9ef2b338652cb8f0d4db73571f8c021
Teodor <thelad43@gmail.com>
2018-01-16 17:03:08 +0200

    Added solution for problem 05. Hands of Cards

---
+++ 14. Dictionaries, Lambda and LINQ - Exercises/05. Hands of Cards/StartUp.cs
@@ -0,0 +1,110 @@
+﻿namespace _05.Hands_of_Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var houseOfCards = new Dictionary<string, Dictionary<int, HashSet<int>>>();
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "JOKER")
+            {
+                var handInfo = inputLine.Split(new char[] { ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var name = handInfo[0];
+
+                if (!houseOfCards.ContainsKey(name))
+                {
+                    houseOfCards.Add(name, new Dictionary<int, HashSet<int>>());
+
+                    for (int i = 1; i <= 4; i++)
+                    {
+                        houseOfCards[name].Add(i, new HashSet<int>());
+                    }
+                }
+
+                for (int i = 1; i < handInfo.Length; i++)
+                {
+                    var currentCard = handInfo[i].Trim();
+                    var face = 0;
+                    var suite = 0;
+
+                    if (currentCard.Length > 2)
+                    {
+                        face = GetFace(currentCard.Substring(0, 2));
+                        suite = GetSuite(currentCard.Substring(2));
+                    }
+                    else
+                    {
+                        face = GetFace(currentCard[0].ToString());
+                        suite = GetSuite(currentCard[1].ToString());
+                    }
+
+                    if (!houseOfCards[name][suite].Contains(face))
+                    {
+                        houseOfCards[name][suite].Add(face);
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var outerPair in houseOfCards)
+            {
+                var sum = 0;
+
+                foreach (var inner in outerPair.Value)
+                {
+                    sum += inner.Key * inner.Value.Sum();
+                }
+
+                Console.WriteLine($"{outerPair.Key}: {sum}");
+            }
+        }
+
+        private static int GetSuite(string suite)
+        {
+            switch (suite)
+            {
+                case "S":
+                    return 4;
+
+                case "H":
+                    return 3;
+
+                case "D":
+                    return 2;
+
+                case "C":
+                    return 1;
+
+                default:
+                    return 0;
+            }
+        }
+
+        private static int GetFace(string face)
+        {
+            switch (face)
+            {
+                case "J":
+                    return 11;
+
+                case "Q":
+                    return 12;
+
+                case "K":
+                    return 13;
+
+                case "A":
+                    return 14;
+
+                default:
+                    return int.Parse(face);
+            }
+        }
+    }
+}
