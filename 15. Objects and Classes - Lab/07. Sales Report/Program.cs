bde3c5b228ce6d92ca7450ed0280ae6e94c9bd6c
Teodor <thelad43@gmail.com>
2018-01-18 17:10:59 +0200

    Added solution for problem 07. Sales Report

---
+++ 15. Objects and Classes - Lab/07. Sales Report/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _07.Sales_Report
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sales = new Sale[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                sales[i] = ReadSale();
+            }
+
+            var result = new SortedDictionary<string, decimal>();
+
+            foreach (var sale in sales)
+            {
+                if (result.ContainsKey(sale.Town))
+                {
+                    result[sale.Town] += sale.Price * (decimal)sale.Quantity;
+                }
+                else
+                {
+                    result[sale.Town] = sale.Price * (decimal)sale.Quantity;
+                }
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value:F2}");
+            }
+        }
+
+        private static Sale ReadSale()
+        {
+            var saleTokens = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var town = saleTokens[0];
+            var product = saleTokens[1];
+            var price = decimal.Parse(saleTokens[2]);
+            var quantity = double.Parse(saleTokens[3]);
+
+            return new Sale(town, product, price, quantity);
+        }
+    }
+}
---
+++ 15. Objects and Classes - Lab/07. Sales Report/Sale.cs
@@ -0,0 +1,66 @@
+﻿namespace _07.Sales_Report
+{
+    public class Sale
+    {
+        private string town;
+        private string product;
+        private decimal price;
+        private double quantity;
+
+        public Sale(string town, string product, decimal price, double quantity)
+        {
+            this.town = town;
+            this.product = product;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public string Town
+        {
+            get
+            {
+                return this.town;
+            }
+            private set
+            {
+                this.town = value;
+            }
+        }
+
+        public string Product
+        {
+            get
+            {
+                return this.product;
+            }
+            private set
+            {
+                this.product = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                this.price = value;
+            }
+        }
+
+        public double Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+            private set
+            {
+                this.quantity = value;
+            }
+        }
+    }
+}
