254b2d553fdd49472123ca6d091a9c486ce97f50
Teodor <thelad43@gmail.com>
2018-01-18 17:07:51 +0200

    Added solution for problem 04. Distance between Points

---
+++ 15. Objects and Classes - Lab/04. Distance between Points/Point.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.Distance_between_Points
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            private set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            private set
+            {
+                this.y = value;
+            }
+        }
+    }
+}
---
+++ 15. Objects and Classes - Lab/04. Distance between Points/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.Distance_between_Points
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstPointTokens = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var secondPointTokens = Console.ReadLine()
+              .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            var firstPoint = new Point(firstPointTokens[0], firstPointTokens[1]);
+            var secondPoint = new Point(secondPointTokens[0], secondPointTokens[1]);
+            var distance = CalculateDistance(firstPoint, secondPoint);
+            Console.WriteLine($"{distance:F3}");
+        }
+
+        private static double CalculateDistance(Point firstPoint, Point secondPoint)
+        {
+            var sideA = secondPoint.X - firstPoint.X;
+            var sideB = secondPoint.Y - firstPoint.Y;
+            var distance = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+            return distance;
+        }
+    }
+}
