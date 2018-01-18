dd0292ef28e561e9df4b9b2ea13369fd74a60725
Teodor <thelad43@gmail.com>
2018-01-18 17:04:28 +0200

    Added solution for problem 01. Day of Week

---
+++ 15. Objects and Classes - Lab/01. Day of Week/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Day_of_Week
+{
+    using System;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var format = "d-M-yyyy";
+            var date = DateTime.ParseExact(input, format, CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
