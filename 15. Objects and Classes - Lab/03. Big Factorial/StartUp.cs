415c41339bd11a0aeaa28960e554ac1259301adc
Teodor <thelad43@gmail.com>
2018-01-18 17:06:35 +0200

    Added solution for problem 03. Big Factorial

---
+++ 15. Objects and Classes - Lab/03. Big Factorial/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.Big_Factorial
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var factorial = CalculateFactorial(n);
+            Console.WriteLine(factorial);
+        }
+
+        private static BigInteger CalculateFactorial(int n)
+        {
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+    }
+}
