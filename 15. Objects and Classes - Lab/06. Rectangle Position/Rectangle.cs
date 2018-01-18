f924da5d7b2def20720757466999a362bd5308ac
Teodor <thelad43@gmail.com>
2018-01-18 17:09:55 +0200

    Added solution for problem 06. Rectangle Position

---
+++ 15. Objects and Classes - Lab/06. Rectangle Position/Rectangle.cs
@@ -0,0 +1,94 @@
+﻿namespace _06.Rectangle_Position
+{
+    public class Rectangle
+    {
+        private int left;
+        private int top;
+        private int width;
+        private int height;
+        private int right;
+        private int bottom;
+
+        public Rectangle(int left, int top, int width, int height, int right, int bottom)
+        {
+            this.Left = left;
+            this.Top = top;
+            this.Width = width;
+            this.Height = height;
+            this.Right = right;
+            this.Bottom = bottom;
+        }
+
+        public int Top
+        {
+            get
+            {
+                return this.top;
+            }
+            private set
+            {
+                this.top = value;
+            }
+        }
+
+        public int Left
+        {
+            get
+            {
+                return this.left;
+            }
+            private set
+            {
+                this.left = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.height = value;
+            }
+        }
+
+        public int Right
+        {
+            get
+            {
+                return this.right;
+            }
+            private set
+            {
+                this.right = value;
+            }
+        }
+
+        public int Bottom
+        {
+            get
+            {
+                return this.bottom;
+            }
+            private set
+            {
+                this.bottom = value;
+            }
+        }
+    }
+}
---
+++ 15. Objects and Classes - Lab/06. Rectangle Position/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace _06.Rectangle_Position
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstRectangle = ReadRectangle();
+            var secondRectangle = ReadRectangle();
+
+            if (IsInside(firstRectangle, secondRectangle))
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Not inside");
+            }
+        }
+
+        private static bool IsInside(Rectangle firstRectangle, Rectangle secondRectangle)
+        {
+            if (firstRectangle.Top <= secondRectangle.Top && firstRectangle.Right <= secondRectangle.Right && firstRectangle.Bottom <= secondRectangle.Bottom && firstRectangle.Left >= secondRectangle.Left)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static Rectangle ReadRectangle()
+        {
+            var inputParams = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rectangle = new Rectangle(inputParams[0], inputParams[1], inputParams[2],
+                inputParams[3], inputParams[0] + inputParams[2],
+                inputParams[1] + inputParams[3]);
+
+            return rectangle;
+        }
+    }
+}
