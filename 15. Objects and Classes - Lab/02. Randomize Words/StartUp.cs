93fa54c7b62797599f62457cd075deb502b682f6
Teodor <thelad43@gmail.com>
2018-01-18 17:05:35 +0200

    Added solution for problem 02. Randomize Words

---
+++ 15. Objects and Classes - Lab/02. Randomize Words/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.Randomize_Words
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var randomGenerator = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var randomNumber = randomGenerator.Next(0, words.Length);
+                var oldValue = words[randomNumber];
+                words[randomNumber] = words[i];
+                words[i] = oldValue;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
