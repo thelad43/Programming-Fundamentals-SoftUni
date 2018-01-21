76e5166e754500be29d1f6aa7fa28a0dca187233
Teodor <thelad43@gmail.com>
2018-01-21 15:34:58 +0200

    Added solution for problem 04. Average Grades

---
+++ 16. Objects and Classes - Exercises/04. Average Grades/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _04.Average_Grades
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ReadStudents()
+                .Where(s => s.AvgGrade >= 5)
+                .OrderBy(s => s.Name)
+                .ThenByDescending(s => s.AvgGrade)
+                .ToList()
+                .ForEach(s => Console.WriteLine($"{s.Name} -> {s.AvgGrade:F2}"));
+        }
+
+        private static Student[] ReadStudents()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var students = new Student[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputParams = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = inputParams[0];
+
+                var grades = inputParams
+                    .Skip(1)
+                    .Select(double.Parse)
+                    .ToList();
+
+                students[i] = new Student(name, grades, grades.Average());
+            }
+
+            return students;
+        }
+    }
+}
---
+++ 16. Objects and Classes - Exercises/04. Average Grades/Student.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace _04.Average_Grades
+{
+    public class Student
+    {
+        private string name;
+        private List<double> grades;
+        private readonly double avgGrade;
+
+        public Student(string name, List<double> grades, double avgGrade)
+        {
+            this.name = name;
+            this.grades = grades;
+            this.avgGrade = avgGrade;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<double> Grades
+        {
+            get
+            {
+                return this.grades;
+            }
+            private set
+            {
+                this.grades = value;
+            }
+        }
+
+        public double AvgGrade
+        {
+            get
+            {
+                return this.avgGrade;
+            }
+        }
+    }
+}
