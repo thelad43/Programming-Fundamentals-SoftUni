d8c0a9564dd8e52febd61c2e4975fb590267b4cb
Teodor <thelad43@gmail.com>
2018-01-21 15:41:10 +0200

    Added solution for problem 08. Mentor Group

---
+++ 16. Objects and Classes - Exercises/08. Mentor Group/StartUp.cs
@@ -0,0 +1,111 @@
+﻿namespace _08.Mentor_Group
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var students = new SortedDictionary<string, Student>();
+            var format = "dd/MM/yyyy";
+
+            while (inputLine != "end of dates")
+            {
+                var inputParams = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = inputParams[0];
+                var dates = new List<DateTime>();
+
+                if (inputParams.Length > 1)
+                {
+                    var dateSequance = inputParams[1]
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < dateSequance.Length; i++)
+                    {
+                        var currentDate = DateTime.ParseExact(dateSequance[i], format,
+                            CultureInfo.InvariantCulture);
+
+                        dates.Add(currentDate);
+                    }
+                }
+
+                var student = new Student
+                {
+                    Name = name
+                };
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, student);
+                    student.AttendanceDates = dates;
+                }
+                else
+                {
+                    students[name].AttendanceDates.AddRange(dates);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            inputLine = Console.ReadLine();
+
+            while (inputLine != "end of comments")
+            {
+                var commentsInfo = inputLine
+                    .Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = commentsInfo[0];
+                var comment = commentsInfo[1];
+                var comments = new List<string>
+                {
+                    comment
+                };
+
+                if (students.ContainsKey(name))
+                {
+                    if (students[name].Comments != null)
+                    {
+                        students[name].Comments.AddRange(comments);
+                    }
+                    else
+                    {
+                        students[name].Comments = comments;
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            PrintStudents(students, format);
+        }
+
+        private static void PrintStudents(SortedDictionary<string, Student> students, string format)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key}{Environment.NewLine}Comments:");
+
+                if (student.Value.Comments != null)
+                {
+                    foreach (var comment in student.Value.Comments)//
+                    {
+                        Console.WriteLine($"- {comment}");
+                    }
+                }
+
+                Console.WriteLine($"Dates attended:");
+
+                foreach (var date in student.Value.AttendanceDates.OrderBy(d => d))
+                {
+                    Console.WriteLine($"-- {date.ToString(format, CultureInfo.InvariantCulture)}");
+                }
+            }
+        }
+    }
+}
---
+++ 16. Objects and Classes - Exercises/08. Mentor Group/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.Mentor_Group
+{
+    public class Student
+    {
+        private string name;
+        private List<string> comments;
+        private List<DateTime> attendanceDates;
+
+        public Student()
+        {
+            this.Name = this.name;
+            this.Comments = this.comments;
+            this.AttendanceDates = this.attendanceDates;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<string> Comments
+        {
+            get
+            {
+                return this.comments;
+            }
+            set
+            {
+                this.comments = value;
+            }
+        }
+
+        public List<DateTime> AttendanceDates
+        {
+            get
+            {
+                return this.attendanceDates;
+            }
+            set
+            {
+                this.attendanceDates = value;
+            }
+        }
+    }
+}
