617c1a3f7dfef1c1a722a59d67c52a34e5de91dc
Teodor <thelad43@gmail.com>
2018-01-21 15:42:30 +0200

    Added solution for problem 09. Teamwork Projects

---
+++ 16. Objects and Classes - Exercises/09. Teamwork Projects/StartUp.cs
@@ -0,0 +1,102 @@
+﻿namespace _09.Teamwork_Projects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var countOfTeams = int.Parse(Console.ReadLine());
+            var teams = new List<Team>();
+
+            for (int i = 0; i < countOfTeams; i++)
+            {
+                var newTeam = Console.ReadLine()
+                    .Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var membersList = new List<string>();
+
+                var team = new Team
+                {
+                    Name = newTeam[1],
+                    CreatorName = newTeam[0],
+                    Members = membersList
+                };
+
+                if (!teams.Select(x => x.Name).Contains(team.Name))
+                {
+                    if (!teams.Select(x => x.CreatorName).Contains(team.CreatorName))
+                    {
+                        teams.Add(team);
+                        Console.WriteLine($"Team {newTeam[1]} has been created by {newTeam[0]}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{team.CreatorName} cannot create another team!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Team {team.Name} was already created!");
+                }
+            }
+
+            var teamRegistration = Console.ReadLine();
+
+            while (!teamRegistration.Equals("end of assignment"))
+            {
+                var tokens = teamRegistration
+                    .Split(new char[] { '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var newUser = tokens[0];
+                var teamName = tokens[1];
+
+                if (!teams.Select(x => x.Name).Contains(teamName))
+                {
+                    Console.WriteLine($"Team {teamName} does not exist!");
+                }
+                else if (teams.Select(x => x.Members).Any(x => x.Contains(newUser)) ||
+                    teams.Select(x => x.CreatorName).Contains(newUser))
+                {
+                    Console.WriteLine($"Member {newUser} cannot join team {teamName}!");
+                }
+                else
+                {
+                    int teamToJoinIndex = teams.FindIndex(x => x.Name == teamName);
+                    teams[teamToJoinIndex].Members.Add(newUser);
+                }
+
+                teamRegistration = Console.ReadLine();
+            }
+
+            var teamsToDisband = teams
+                .OrderBy(x => x.Name)
+                .Where(x => x.Members.Count == 0);
+
+            var fullTeams = teams
+                .OrderByDescending(x => x.Members.Count)
+                .ThenBy(x => x.Name)
+                .Where(x => x.Members.Count > 0);
+
+            foreach (var team in fullTeams)
+            {
+                Console.WriteLine($"{team.Name}");
+                Console.WriteLine($"- {team.CreatorName}");
+
+                foreach (var member in team.Members.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {member}");
+                }
+            }
+
+            Console.WriteLine("Teams to disband:");
+
+            foreach (var item in teamsToDisband)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
---
+++ 16. Objects and Classes - Exercises/09. Teamwork Projects/Team.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace _09.Teamwork_Projects
+{
+    public class Team
+    {
+        private string name;
+        private string creatorName;
+        private List<string> members;
+
+        public Team()
+        {
+            this.Name = this.name;
+            this.CreatorName = this.creatorName;
+            this.Members = this.members;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string CreatorName
+        {
+            get
+            {
+                return this.creatorName;
+            }
+            set
+            {
+                this.creatorName = value;
+            }
+        }
+
+        public List<string> Members
+        {
+            get
+            {
+                return this.members;
+            }
+            set
+            {
+                this.members = value;
+            }
+        }
+    }
+}
