23741f4ec9dcccdacc89d58ab1d333883a269f1d
Teodor <thelad43@gmail.com>
2018-01-21 15:36:35 +0200

    Added solution for problem 05. Book Library

---
+++ 16. Objects and Classes - Exercises/05. Book Library/Library.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace _05.Book_Library
+{
+    public class Library
+    {
+        private string name;
+        private List<Book> books;
+
+        public Library(string name, List<Book> books)
+        {
+            this.name = name;
+            this.books = books;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<Book> Books
+        {
+            get
+            {
+                return this.books;
+            }
+            private set
+            {
+                this.books = value;
+            }
+        }
+    }
+}
---
+++ 16. Objects and Classes - Exercises/05. Book Library/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace _05.Book_Library
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var books = ReadBooks();
+            var library = new Library("Library", books);
+            var salesByAuthor = new Dictionary<string, decimal>();
+
+            foreach (var author in books.Select(x => x.Author).Distinct())
+            {
+                salesByAuthor.Add(author, books.Where(x => x.Author == author)
+                                               .Select(x => x.Price)
+                                               .Sum());
+            }
+
+            foreach (var kvp in salesByAuthor
+                                .OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value:f2}");
+            }
+        }
+
+        private static List<Book> ReadBooks()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var books = new List<Book>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                var bookTokens = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var format = "dd.MM.yyyy";
+                var title = bookTokens[0];
+                var author = bookTokens[1];
+                var publisher = bookTokens[2];
+                var releaseDate = DateTime.ParseExact(bookTokens[3], format, CultureInfo.InvariantCulture);
+                var ISBNNumber = bookTokens[4];
+                var price = decimal.Parse(bookTokens[5]);
+
+                var book = new Book(title, author, publisher, releaseDate, ISBNNumber, price);
+                books.Add(book);
+            }
+
+            return books;
+        }
+    }
+}
