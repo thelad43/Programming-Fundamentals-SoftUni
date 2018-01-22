9597a3bfc7edfeb2b7c14bb15f4481d62dacb77d
Teodor <thelad43@gmail.com>
2018-01-22 19:22:51 +0200

    Added solution for problem 04. Merge Files

---
+++ 17. Files and Exceptions - Lab/04. Merge Files/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.Merge_Files
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            if (!File.Exists("../../Output.txt"))
+            {
+                var file = File.Create("../../Output.txt");
+                file.Close();
+            }
+
+            var allData = File.ReadAllLines("../../FileOne.txt");
+            allData = allData.Concat(File.ReadAllLines("../../FileTwo.txt")).ToArray();
+            File.WriteAllLines(@"../../Output.txt", allData.OrderBy(x => x));
+        }
+    }
+}
