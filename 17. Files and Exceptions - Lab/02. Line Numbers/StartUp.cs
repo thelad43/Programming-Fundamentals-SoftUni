d2e6bd3948497fca21bb5a6be27f93e4ee868f6d
Teodor <thelad43@gmail.com>
2018-01-22 19:18:59 +0200

    Added solution for problem 02. Line Numbers

---
+++ 17. Files and Exceptions - Lab/02. Line Numbers/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.Line_Numbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allLines = File.ReadAllLines("../../Input.txt");
+
+            if (!File.Exists("../../Output.txt"))
+            {
+                var file = File.Create("../../Output.txt");
+                file.Close();
+            }
+
+            StringBuilder numberedLines = new StringBuilder(allLines.Length);
+
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                numberedLines.Append(i + 1);
+                numberedLines.AppendLine($". {allLines[i]}");
+            }
+
+            File.WriteAllLines("../../Output.txt", numberedLines.ToString().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
