c93767893e407db87b273c771dd0e9402d9cfd71
Teodor <thelad43@gmail.com>
2018-01-22 19:20:30 +0200

    Added solution for problem 03. Word Count

---
+++ 17. Files and Exceptions - Lab/03. Word Count/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.Word_Count
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = File.ReadAllText("../../words.txt")
+                 .ToLower()
+                 .Split(' ');
+
+            var text = File.ReadAllText("../../text.txt")
+                .ToLower()
+                .Split(new char[] { '\n', '\r', ' ', '.', ',', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!File.Exists("../../Output.txt"))
+            {
+                var file = File.Create("../../Output.txt");
+                file.Close();
+            }
+
+            var givenWordsCount = new List<string>();
+            var containedWords = CountWords(words, text);
+
+            foreach (var kvp in containedWords)
+            {
+                givenWordsCount.Add($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            File.WriteAllLines("../../Output.txt", givenWordsCount);
+        }
+
+        private static Dictionary<string, int> CountWords(string[] words, string[] text)
+        {
+            var containedWords = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                containedWords[word] = 0;
+            }
+
+            foreach (var word in text)
+            {
+                if (containedWords.ContainsKey(word))
+                {
+                    containedWords[word]++;
+                }
+            }
+
+            return containedWords
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, y => y.Value);
+        }
+    }
+}
