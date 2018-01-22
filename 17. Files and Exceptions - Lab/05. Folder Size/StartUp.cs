3ee25722e255c8910ca4c6082d82c5642c3d699b
Teodor <thelad43@gmail.com>
2018-01-22 19:25:00 +0200

    Added solution for problem 05. Folder Size

---
+++ 17. Files and Exceptions - Lab/05. Folder Size/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.Folder_Size
+{
+    using System;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            if (!File.Exists("../../Size of Files in MB.txt"))
+            {
+                var file = File.Create("../../Size of Files in MB.txt");
+                file.Close();
+            }
+
+            var files = Directory.GetFiles("../../TestFolder");
+            var sum = 0D;
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = (sum / 1024) / 1024;
+            File.WriteAllText("../../Size of Files in MB.txt", sum.ToString());
+        }
+    }
+}
