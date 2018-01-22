1e5adb301cd83954fdbc5e3cafbe959cba727e5b
Teodor <thelad43@gmail.com>
2018-01-22 19:17:24 +0200

    Added solution for problem 01. Odd Lines

---
+++ 17. Files and Exceptions - Lab/01. Odd Lines/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.Odd_Lines
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allLines = File.ReadAllLines("../../Input.txt");
+
+            if (!File.Exists("../../Output.txt"))
+            {
+                var file = File.Create("../../Output.txt");
+                file.Close();
+            }
+
+            var oddLines = new List<string>();
+
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddLines.Add(allLines[i]);
+                }
+            }
+
+            File.WriteAllLines("../../Output.txt", oddLines);
+        }
+    }
+}
