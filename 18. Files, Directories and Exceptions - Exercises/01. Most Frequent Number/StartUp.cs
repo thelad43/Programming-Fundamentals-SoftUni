a8febe56465ab69f809d8d8214e2134242a22efa
Teodor <thelad43@gmail.com>
2018-01-23 17:51:40 +0200

    Added solution for problem 01. Most Frequent Number

---
+++ 18. Files, Directories and Exceptions - Exercises/01. Most Frequent Number/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.Most_Frequent_Number
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = File.ReadAllText("../../input.txt")
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var mostFrequentElement = numbers.GroupBy(item => item).OrderByDescending(g => g.Count()).Select(g => g.Key).First();
+
+            if (!File.Exists("../../output.txt"))
+            {
+                var file = File.Create("../../output.txt");
+                file.Close();
+            }
+
+            File.WriteAllText("../../output.txt", mostFrequentElement.ToString());
+        }
+    }
+}
