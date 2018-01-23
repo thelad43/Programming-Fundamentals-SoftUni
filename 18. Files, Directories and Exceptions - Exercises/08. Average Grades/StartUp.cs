271b13b1e7e98e5879c5448c7e4c7566ff4c6bc4
Teodor <thelad43@gmail.com>
2018-01-23 18:09:41 +0200

    Added solution for problem 08. Average Grades

---
+++ 18. Files, Directories and Exceptions - Exercises/08. Average Grades/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace _08.Average_Grades
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var students = ReadStudents();
+
+            students = students
+                .Where(x => x.AvgGrade >= 5)
+                .OrderBy(x => x.Name)
+                .ThenBy(x => -x.AvgGrade)
+                .ToList();
+
+            StudentsToFile(students);
+        }
+
+        private static void StudentsToFile(List<Student> students)
+        {
+            if (!File.Exists("../../output.txt"))
+            {
+                var file = File.Create("../../output.txt");
+                file.Close();
+            }
+
+            File.WriteAllLines("../../output.txt", students.Select(x => $"{x.Name} -> {x.AvgGrade:F2}").ToList());
+        }
+
+        private static List<Student> ReadStudents()
+        {
+            var studentGrades = File.ReadAllLines("../../input.txt");
+            var students = new List<Student>();
+
+            for (int i = 0; i < studentGrades.Length; i++)
+            {
+                var inputParams = studentGrades[i]
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = inputParams[0];
+
+                var grades = inputParams
+                    .Skip(1)
+                    .Select(double.Parse)
+                    .ToList();
+
+                students.Add(new Student(name, grades, grades.Average()));
+            }
+
+            return students;
+        }
+    }
+}
---
+++ 18. Files, Directories and Exceptions - Exercises/08. Average Grades/Student.cs
@@ -0,0 +1,51 @@
+﻿namespace _08.Average_Grades
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Student
+    {
+        private string name;
+        private List<double> grades;
+        private readonly double avgGrade;
+
+        public Student(string name, List<double> grades, double avgGrade)
+        {
+            this.name = name;
+            this.grades = grades;
+            this.avgGrade = avgGrade;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<double> Grades
+        {
+            get
+            {
+                return this.grades;
+            }
+            private set
+            {
+                this.grades = value;
+            }
+        }
+
+        public double AvgGrade
+        {
+            get
+            {
+                return this.avgGrade;
+            }
+        }
+    }
+}
