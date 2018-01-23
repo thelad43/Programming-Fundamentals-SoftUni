13f01f93ede096c2d1cf90ca77f9995f9c922e57
Teodor <thelad43@gmail.com>
2018-01-23 18:02:05 +0200

    Added solution for problem 05. A Miner Task

---
+++ 18. Files, Directories and Exceptions - Exercises/05. A Miner Task/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace _05.A_Miner_Task
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var resources = new Dictionary<string, long>();
+            var allLinesOfInput = File.ReadAllLines("../../input.txt");
+            var resource = allLinesOfInput[0];
+            resources[resource] = 0;
+
+            for (int i = 1; i < allLinesOfInput.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    resource = allLinesOfInput[i];
+
+                    if (resource == "stop")
+                    {
+                        break;
+                    }
+
+                    if (!resources.ContainsKey(resource))
+                    {
+                        resources[resource] = 0;
+                    }
+                }
+                else
+                {
+                    resources[resource] += long.Parse(allLinesOfInput[i]);
+                }
+            }
+
+            ResourcesToFile(resources);
+        }
+
+        private static void ResourcesToFile(Dictionary<string, long> resources)
+        {
+            if (!File.Exists("../../output.txt"))
+            {
+                var file = File.Create("../../output.txt");
+                file.Close();
+            }
+
+            var result = new StringBuilder();
+
+            foreach (var resource in resources)
+            {
+                result.AppendLine($"{resource.Key} -> {resource.Value}");
+            }
+
+            File.WriteAllText("../../output.txt", result.ToString());
+        }
+    }
+}
