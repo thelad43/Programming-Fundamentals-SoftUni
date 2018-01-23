2efe28c1d01ebc48df448d836bfa26a7f92f86b3
Teodor <thelad43@gmail.com>
2018-01-23 17:53:49 +0200

    Added solution for problem 02. Index of Letters

---
+++ 18. Files, Directories and Exceptions - Exercises/02. Index of Letters/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.Index_of_Letters
+{
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var word = File.ReadAllText("../../input.txt").ToLower();
+
+            if (!File.Exists("../../output.txt"))
+            {
+                var file = File.Create("../../output.txt");
+                file.Close();
+            }
+
+            var result = new string[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                result[i] = $"{word[i]} -> {word[i] - 'a'}";
+            }
+
+            File.WriteAllLines("../../output.txt", result);
+        }
+    }
+}
