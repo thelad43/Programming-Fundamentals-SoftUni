b0df553d16bbe036a8f4cd9af609df8c6b88c882
Teodor <thelad43@gmail.com>
2018-01-26 18:34:59 +0200

    Added solution for problem 02. Count Substring Occurrences

---
+++ 19. Strings and Text Processing - Lab/02. Count Substring Occurrences/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.Count_Substring_Occurrences
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().ToLower();
+            var textForSearching = Console.ReadLine().ToLower();
+            var occurrencesCount = CountSubstringOccurrences(text, textForSearching);
+            Console.WriteLine(occurrencesCount);
+        }
+
+        private static int CountSubstringOccurrences(string text, string textForSearching)
+        {
+            var occurrencesCount = 0;
+            var index = text.IndexOf(textForSearching);
+
+            while (index != -1)
+            {
+                occurrencesCount++;
+                index = text.IndexOf(textForSearching, index + 1);
+            }
+
+            return occurrencesCount;
+        }
+    }
+}
