e261e030257181a7dbead177e1808e53be51bbf1
Teodor <thelad43@gmail.com>
2018-01-26 18:34:09 +0200

    Added solution for problem 01. Reverse String

---
+++ 19. Strings and Text Processing - Lab/01. Reverse String/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.Reverse_String
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var textForReversing = Console.ReadLine();
+            var reversed = ReverseString(textForReversing);
+            Console.WriteLine(reversed);
+        }
+
+        private static string ReverseString(string textForReversing)
+        {
+            var sb = new StringBuilder(textForReversing.Length);
+
+            for (int i = textForReversing.Length - 1; i >= 0; i--)
+            {
+                sb.Append(textForReversing[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
