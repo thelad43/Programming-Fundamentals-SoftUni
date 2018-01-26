beec41274ba092c2b8bc9894557d7bbd3c114a71
Teodor <thelad43@gmail.com>
2018-01-26 18:36:54 +0200

    Added solution for problem 04. Palindromes

---
+++ 19. Strings and Text Processing - Lab/04. Palindromes/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _04.Palindromes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var delimiters = new char[] { ' ', ',', '.', '?', '!' };
+            var words = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var palindromes = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (IsPalindrome(word))
+                {
+                    palindromes.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", palindromes.Distinct().OrderBy(w => w)));
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            var isPalindrome = true;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!(word[i] == word[word.Length - 1 - i]))
+                {
+                    return false;
+                }
+            }
+
+            return isPalindrome;
+        }
+    }
+}
