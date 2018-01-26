7ae351e7988f4bcb09bb6c95ccc66f12cce404f0
Teodor <thelad43@gmail.com>
2018-01-26 18:35:52 +0200

    Added solution for problem 03. Text Filter

---
+++ 19. Strings and Text Processing - Lab/03. Text Filter/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.Text_Filter
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var bannedWords = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var text = Console.ReadLine();
+
+            foreach (var banWord in bannedWords)
+            {
+                text = text.Replace(banWord, new string('*', banWord.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
