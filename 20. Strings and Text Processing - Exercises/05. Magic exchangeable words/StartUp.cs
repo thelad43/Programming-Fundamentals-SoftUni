919d790976109c14bc9b2298e836bf931cfb164a
Teodor <thelad43@gmail.com>
2018-01-27 17:03:24 +0200

    Added solution for problem 05. Magic exchangeable words

---
+++ 20. Strings and Text Processing - Exercises/05. Magic exchangeable words/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.Magic_exchangeable_words
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var firstWord = new HashSet<char>(words[0]);
+            var secondWord = new HashSet<char>(words[1]);
+            Console.WriteLine((firstWord.Count == secondWord.Count) ? "true" : "false");
+        }
+    }
+}
