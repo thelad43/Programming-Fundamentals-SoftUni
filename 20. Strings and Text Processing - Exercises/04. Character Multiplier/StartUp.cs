e8aecd5459da08e827e69250a0006bbd296f5c26
Teodor <thelad43@gmail.com>
2018-01-27 17:01:18 +0200

    Added solution for problem 04. Character Multiplier

---
+++ 20. Strings and Text Processing - Exercises/04. Character Multiplier/StartUp.cs
@@ -0,0 +1,80 @@
+﻿namespace _04.Character_Multiplier
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputParams = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var sum = GetSumOfStrings(inputParams[0], inputParams[1]);
+            Console.WriteLine(sum);
+        }
+
+        private static long GetSumOfStrings(string firstString, string secondString)
+        {
+            var sum = 0L;
+
+            if (firstString.CompareTo(secondString) != 0)
+            {
+                sum = GetSumOfStringsWithDifferentLengths(firstString, secondString);
+            }
+            else
+            {
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    sum += firstString[i] * secondString[i];
+                }
+            }
+
+            return sum;
+        }
+
+        private static long GetSumOfStringsWithDifferentLengths(string firstString, string secondString)
+        {
+            var sum = 0L;
+
+            if (firstString.Length > secondString.Length)
+            {
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    try
+                    {
+                        var currentChar = secondString[i];
+                        sum += firstString[i] * secondString[i];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        for (int k = i; k < firstString.Length; k++)
+                        {
+                            sum += firstString[k];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < secondString.Length; i++)
+                {
+                    try
+                    {
+                        var currentChar = firstString[i];
+                        sum += firstString[i] * secondString[i];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        for (int k = i; k < secondString.Length; k++)
+                        {
+                            sum += secondString[k];
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
