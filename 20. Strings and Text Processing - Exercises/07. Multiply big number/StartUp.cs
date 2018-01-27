62a9c5e9eac27a13a3925531a2ca7c94e82c807b
Teodor <thelad43@gmail.com>
2018-01-27 17:05:11 +0200

    Added solution for problem 07. Multiply big number

---
+++ 20. Strings and Text Processing - Exercises/07. Multiply big number/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _07.Multiply_big_number
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstNumber = Console.ReadLine().TrimStart('0');
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber == "0" || secondNumber == 0 || string.IsNullOrEmpty(firstNumber))
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var product = 0;
+            var numberInMind = 0;
+            var remainder = 0;
+            var result = new StringBuilder();
+
+            for (int i = firstNumber.Length - 1; i >= 0; i--)
+            {
+                product = int.Parse(firstNumber[i].ToString()) * secondNumber + numberInMind;
+                numberInMind = (product / 10);
+                remainder = (product % 10);
+                result.Append(remainder);
+
+                if (i == 0 && numberInMind != 0)
+                {
+                    result.Append(numberInMind);
+                }
+            }
+
+            var resultToCharArr = result.ToString().ToCharArray();
+            Array.Reverse(resultToCharArr);
+            Console.WriteLine(new string(resultToCharArr));
+        }
+    }
+}
