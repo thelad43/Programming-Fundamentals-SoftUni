aeaa370cfe94fe407a69c1d8e30d3ba665ba8207
Teodor <thelad43@gmail.com>
2018-01-27 17:00:06 +0200

    Added solution for problem 03. Unicode Characters

---
+++ 20. Strings and Text Processing - Exercises/03. Unicode Characters/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.Unicode_Characters
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = ConvertToUnicodeCharacterLiterals(input);
+            Console.WriteLine(result);
+        }
+
+        private static string ConvertToUnicodeCharacterLiterals(string input)
+        {
+            var symbols = input.ToCharArray();
+            var result = new StringBuilder();
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                var cuurentUnicode = GetEscapeSequence(symbols[i]);
+                result.Append(cuurentUnicode);
+            }
+
+            return result.ToString().ToLower();
+        }
+
+        private static string GetEscapeSequence(char c)
+        {
+            return "\\u" + ((int)c).ToString("X4");
+        }
+    }
+}
