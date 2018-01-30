6bb9ad0d58c7dcc9ba4b0534c0aa87f07fd5129b
Teodor <thelad43@gmail.com>
2018-01-30 11:12:50 +0200

    Added solution for problem 03. Match Hexadecimal Numbers

---
+++ 21. Regular Expressions (RegEx) - Lab/03. Match Hexadecimal Numbers/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Match_Hexadecimal_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var hexadecimalNumbers = Console.ReadLine();
+            var pattern = @"\b(?:0x)?[\dA-F]+\b";
+            var regex = new Regex(pattern);
+
+            var matches = regex
+                .Matches(hexadecimalNumbers)
+                .Cast<Match>()
+                .Select(m => m.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
