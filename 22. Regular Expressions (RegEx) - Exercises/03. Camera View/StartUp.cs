49fa19334c7d6acb9ef4037201c544a98710b5e4
Teodor <thelad43@gmail.com>
2018-01-30 17:35:52 +0200

    Added solution for problem 03. Camera View

---
+++ 22. Regular Expressions (RegEx) - Exercises/03. Camera View/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.Camera_View
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputParams = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var skip = inputParams[0];
+            var take = inputParams[1];
+            var text = Console.ReadLine();
+            var regex = @"(?<=\|<.{" + skip + "})([^|]{0," + take + "})";
+            var words = Regex.Matches(text, regex);
+            var result = new List<string>();
+
+            foreach (Match word in words)
+            {
+                result.Add(word.Groups[1].Value);
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
