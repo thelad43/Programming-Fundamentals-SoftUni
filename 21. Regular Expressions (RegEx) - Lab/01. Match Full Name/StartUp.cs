77f2e12ae8cd8d18d09d070d1281a4d140d4d33e
Teodor <thelad43@gmail.com>
2018-01-30 11:10:55 +0200

    Added solution for problem 01. Match Full Name

---
+++ 21. Regular Expressions (RegEx) - Lab/01. Match Full Name/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Match_Full_Name
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine();
+            var pattern = @"\b([A-Z]{1}[a-z]{1,10}) ([A-Z]{1}[a-z]{1,10})\b";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(names);
+
+            foreach (var match in matches)
+            {
+                Console.Write($"{match} ");
+            }
+        }
+    }
+}
