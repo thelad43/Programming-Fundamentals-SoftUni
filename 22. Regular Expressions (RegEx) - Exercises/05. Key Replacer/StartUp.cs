61cfb428463b7309af594eca0c235b1eb3a8aeb3
Teodor <thelad43@gmail.com>
2018-01-30 17:37:53 +0200

    Added solution for problem 05. Key Replacer

---
+++ 22. Regular Expressions (RegEx) - Exercises/05. Key Replacer/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.Key_Replacer
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var key = Console.ReadLine();
+            var text = Console.ReadLine();
+            var pattern = @"^(?<startKey>[a-zA-Z]*)(?:\<|\||\\)(?:.+)(?:\<|\||\\)(?<endKey>[a-zA-Z]*)$";
+            var keyMatch = Regex.Match(key, pattern);
+
+            if (keyMatch.Success)
+            {
+                var startKey = keyMatch.Groups["startKey"].Value;
+                var endKey = keyMatch.Groups["endKey"].Value;
+                var textPattern = $@"{startKey}(?<word>.*?){endKey}";
+                var matches = Regex.Matches(text, textPattern);
+                var result = new StringBuilder();
+
+                foreach (Match match in matches)
+                {
+                    result.Append(match.Groups["word"].Value);
+                }
+
+                Console.WriteLine(result.ToString().Length == 0 ? "Empty result" : result.ToString());
+            }
+        }
+    }
+}
