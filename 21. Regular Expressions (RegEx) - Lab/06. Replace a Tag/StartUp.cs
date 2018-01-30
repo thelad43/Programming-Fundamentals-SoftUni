87e5479c0ef5d6c14f9006fee4ef814209815f9f
Teodor <thelad43@gmail.com>
2018-01-30 11:16:31 +0200

    Added solution for problem 06. Replace a Tag

---
+++ 21. Regular Expressions (RegEx) - Lab/06. Replace a Tag/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.Replace_a_Tag
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            var newPattern = @"[URL href=$1]$2[/URL]";
+
+            while (inputLine != "end")
+            {
+                Console.WriteLine(Regex.Replace(inputLine, pattern, newPattern));
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
