9acb14e47b42766a116fc10562a1c233b41ac3e5
Teodor <thelad43@gmail.com>
2018-01-30 11:15:31 +0200

    Added solution for problem 05. Match Numbers

---
+++ 21. Regular Expressions (RegEx) - Lab/05. Match Numbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.Match_Numbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine();
+            var pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(numbers);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}
