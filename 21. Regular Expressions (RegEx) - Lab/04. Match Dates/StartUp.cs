77772cf69cf8a2fbe9716a360f4216cf2734a770
Teodor <thelad43@gmail.com>
2018-01-30 11:14:34 +0200

    Added solution for problem 04. Match Dates

---
+++ 21. Regular Expressions (RegEx) - Lab/04. Match Dates/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.Match_Dates
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dates = Console.ReadLine();
+            var pattern = @"\b(?<day>\d{2})([.\/-])(?<month>[A-Z]{1}[a-z]{2})\1(?<year>\d{4})\b";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(dates);
+
+            foreach (Match match in matches)
+            {
+                var day = match.Groups["day"].Value;
+                var month = match.Groups["month"].Value;
+                var year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
