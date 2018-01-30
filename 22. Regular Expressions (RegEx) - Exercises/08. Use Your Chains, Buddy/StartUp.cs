cf03172b642285465b258c1a3c1d29084f6216e3
Teodor <thelad43@gmail.com>
2018-01-30 17:40:34 +0200

    Added solution for problem 08. Use Your Chains, Buddy

---
+++ 22. Regular Expressions (RegEx) - Exercises/08. Use Your Chains, Buddy/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _08.Use_Your_Chains__Buddy
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var pattern = @"<p>(.+?)<\/p>";
+            var regex = new Regex(pattern);
+            var matchesAsString = string.Empty;
+
+            foreach (Match match in regex.Matches(text))
+            {
+                matchesAsString += match.Groups[1].Value;
+            }
+
+            matchesAsString = Regex.Replace(matchesAsString, @"[^a-z\d]", " ");
+            matchesAsString = Regex.Replace(matchesAsString, @"\s+|\n+", " ");
+
+            var result = new StringBuilder(matchesAsString.Length);
+
+            foreach (var ch in matchesAsString)
+            {
+                if (ch >= 'a' && ch <= 'm')
+                {
+                    result.Append((char)(ch + 13));
+                }
+                else if (ch >= 'n' && ch <= 'z')
+                {
+                    result.Append((char)(ch - 13));
+                }
+                else
+                {
+                    result.Append(ch);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
