a58c901ae06842c2d7f8a22c18a3305c7fb56b0f
Teodor <thelad43@gmail.com>
2018-01-30 17:33:54 +0200

    Added solution for problem 01. Extract Emails

---
+++ 22. Regular Expressions (RegEx) - Exercises/01. Extract Emails/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.Extract_Emails
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+            var regex = new Regex(pattern);
+
+            var matches = regex
+                .Matches(inputLine)
+                .Cast<Match>()
+                .Select(m => m.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, matches));
+        }
+    }
+}
