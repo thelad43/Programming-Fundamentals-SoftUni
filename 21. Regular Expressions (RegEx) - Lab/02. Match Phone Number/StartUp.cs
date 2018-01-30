57ed2a0fe45b58de109f1114c170694255550d5b
Teodor <thelad43@gmail.com>
2018-01-30 11:11:48 +0200

    Added solution for problem 02. Match Phone Number

---
+++ 21. Regular Expressions (RegEx) - Lab/02. Match Phone Number/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.Match_Phone_Number
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var telephoneNumbers = Console.ReadLine();
+            var pattern = @"\+359([ |-])2\1\d{3}\1\d{4}\b";
+            var regex = new Regex(pattern);
+
+            var matches = regex
+                .Matches(telephoneNumbers)
+                .Cast<Match>()
+                .Select(m => m.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
