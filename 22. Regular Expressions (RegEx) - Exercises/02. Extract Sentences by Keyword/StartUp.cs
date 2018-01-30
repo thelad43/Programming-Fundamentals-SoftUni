11b16be27adb59f80a55cdcea719ef7d4cb08e87
Teodor <thelad43@gmail.com>
2018-01-30 17:34:53 +0200

    Added solution for problem 02. Extract Sentences by Keyword

---
+++ 22. Regular Expressions (RegEx) - Exercises/02. Extract Sentences by Keyword/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.Extract_Sentences_by_Keyword
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var word = Console.ReadLine();
+            var pattern = $"[^.!?;]*(({word}\\W)|(\\W{word}\\W))[^.!?;]*";
+            var text = Console.ReadLine();
+            var regex = new Regex(pattern);
+            var matches = Regex.Matches(text, pattern);
+            var count = 0;
+
+            foreach (Match match in matches)
+            {
+                if (count == 0)
+                {
+                    Console.WriteLine(match);
+                }
+                else
+                {
+                    var sentence = Convert.ToString(match);
+                    Console.WriteLine(sentence.Substring(1));
+                }
+
+                count++;
+            }
+        }
+    }
+}
